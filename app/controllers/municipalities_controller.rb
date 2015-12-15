7caa8393560c30b48bdfd69f2dd948d47ba9cc1c
martent <marten@thavenius.se>
2015-12-15 12:04:27 +0100

    Bug fixes for municipalities

--- app/controllers/municipalities_controller.rb
+++ app/controllers/municipalities_controller.rb
@@ -1,48 +1,48 @@
 class MunicipalitiesController < ApplicationController
-  before_action :set_municipalities, only: [:show, :edit, :update, :destroy]
+  before_action :set_municipality, only: [:edit, :update, :destroy]
 
   def index
     @municipalities = Municipality.order(:name)
   end
 
   def new
-    @municipalities = Municipality.new
+    @municipality = Municipality.new
   end
 
   def edit
   end
 
   def create
-    @municipalities = Municipality.new(municipalities_params)
+    @municipality = Municipality.new(municipality_params)
 
-    if @municipalities.save
-      redirect_to municipalities_path, notice: 'Målgruppen skapades'
+    if @municipality.save
+      redirect_to municipalities_path, notice: 'Kommunen skapades'
     else
       render :new
     end
   end
 
   def update
-    if @municipalities.update(municipalities_params)
-      redirect_to municipalities_path, notice: 'Målgruppen uppdaterades'
+    if @municipality.update(municipality_params)
+      redirect_to municipalities_path, notice: 'Kommunen uppdaterades'
     else
       render :edit
     end
   end
 
   def destroy
-    @municipalities.destroy
-    redirect_to municipalities_url, notice: 'Målgruppen raderades'
+    @municipality.destroy
+    redirect_to municipalities_url, notice: 'Kommunen raderades'
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_municipalities
-      @municipalities = Municipality.find(params[:id])
+    def set_municipality
+      @municipality = Municipality.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def municipalities_params
-      params.require(:municipalities).permit(:name)
+    def municipality_params
+      params.require(:municipality).permit(:name)
     end
 end
