bdcbc5e754c68ba67af2eaa0206953a0163484e3
martent <marten@thavenius.se>
2015-12-15 21:30:21 +0100

    Added to stats

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -2,6 +2,7 @@ class StatisticsController < ApplicationController
   def index
     @refugees = Refugee.count
     @genders = Gender.all.map { |g| "#{Refugee.where(gender: g).count} är #{g.name.downcase}" }.join(', ')
+    @registered_last_seven_days = Refugee.where(registered: 7.days.ago..DateTime.now).count
 
     @without_placement = Refugee.includes(:placements).where(placements: { refugee_id: nil }).count
     @without_residence_permit = Refugee.where(residence_permit_at: nil).count
