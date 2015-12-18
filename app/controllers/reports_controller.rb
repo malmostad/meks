f9eba9013798038146a57b68693b89766cada401
martent <marten@thavenius.se>
2015-12-18 16:28:38 +0100

    Formatting of reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -13,52 +13,52 @@ class ReportsController < ApplicationController
         'Boende',
         'Placerad',
         'Utskriven',
+        'Total placeringstid (dagar)',
         'Anledning till utskrivning',
-        'Kommentar till utskrivning',
-        'Total placeringstid (dagar)'
-      ], style: @heading_style
+        'Kommentar till utskrivning'
+      ],
+      style: @heading_style
 
       Placement.includes(
         :refugee, :home, :moved_out_reason, refugee: [:dossier_numbers, :ssns]).find_each do |placement|
-        placement_data = [
+        sheet.add_row([
           placement.refugee.name,
           placement.refugee.dossier_numbers.map(&:name).join(', '),
           placement.refugee.ssns.map(&:name).join(', '),
           placement.home.name,
           placement.moved_in_at,
           placement.moved_out_at,
+          placement.placement_time,
           placement.moved_out_reason.present? ? placement.moved_out_reason.name : '',
-          placement.comment,
-          placement.placement_time
-        ]
-        sheet.add_row placement_data,
-          style: [
-            @body_style,
-            @body_style,
-            @body_style,
-            @body_style,
-            @date_format,
-            @date_format,
-            @body_style,
-            @wrap,
-            @body_style
-          ],
-          types: [
-            :string,
-            :string,
-            :string,
-            :string,
-            :date,
-            :date,
-            :string,
-            :string,
-            :integer
-          ]
+          placement.comment
+        ],
+        style: [
+          @cell_style,
+          @cell_style,
+          @cell_style,
+          @cell_style,
+          @date_format,
+          @date_format,
+          @cell_style,
+          @cell_style,
+          @wrap_style
+        ],
+        types: [
+          :string,
+          :string,
+          :string,
+          :string,
+          :date,
+          :date,
+          :integer,
+          :string,
+          :string
+        ])
       end
       sheet.column_info.each { |c| c.width = 20 }
       sheet.column_info[4].width = 12
       sheet.column_info[5].width = 12
-      sheet.column_info[7].width = 40
+      sheet.column_info[8].width = 40
     end
     send_xlsx(@axlsx.to_stream.read, 'Placeringar')
   end
@@ -82,7 +82,7 @@ class ReportsController < ApplicationController
 
       Refugee.includes(
           :countries, :ssns, :dossier_numbers, :gender, :placements, :homes).find_each do |refugee|
-        refugee_data = [
+        sheet.add_row([
           refugee.name,
           refugee.registered,
           refugee.deregistered,
@@ -95,21 +95,39 @@ class ReportsController < ApplicationController
           refugee.placements.where(moved_out_at: nil).map(&:home).map(&:name).join(', '),
           refugee.homes.map(&:name).join(', '),
           refugee.total_placement_time
-        ]
-        sheet.add_row refugee_data,
-          style: @body_style,
-          types: [
-            :string,
-            :date,
-            :date,
-            :string,
-            :string,
-            :string,
-            :string,
-            :string,
-            :string,
-            :integer
-          ]
+        ],
+        style: [
+          @cell_style,
+          @date_format,
+          @date_format,
+          @cell_style,
+          @cell_style,
+          @cell_style,
+          @cell_style,
+          @cell_style,
+          @cell_style,
+          @cell_style,
+          @wrap_style,
+          @cell_style
+        ],
+        types: [
+          :string,
+          :date,
+          :date,
+          :string,
+          :string,
+          :string,
+          :string,
+          :string,
+          :string,
+          :string,
+          :string,
+          :integer
+        ])
+        sheet.column_info.each { |c| c.width = 20 }
+        sheet.column_info[6].width = 8
+        sheet.column_info[10].width = 25
+        sheet.column_info[11].width = 12
       end
     end
     send_xlsx(@axlsx.to_stream.read, 'Ensamkommand-barn')
@@ -118,10 +136,10 @@ class ReportsController < ApplicationController
   private
 
   def create_xlsx
+    @axlsx = Axlsx::Package.new
+
     font_name = 'Calibri'
     align = { vertical: :top }
-
-    @axlsx = Axlsx::Package.new
 
     @heading_style = @axlsx.workbook.styles.add_style(
       font_name: font_name,
@@ -130,13 +148,12 @@ class ReportsController < ApplicationController
       alignment: align,
       width: 10
     )
-    @body_style = @axlsx.workbook.styles.add_style(
+    @cell_style = @axlsx.workbook.styles.add_style(
       font_name: font_name,
       fg_color: '000000',
       alignment: align,
-      width: 10
     )
-    @wrap = @axlsx.workbook.styles.add_style(
+    @wrap_style = @axlsx.workbook.styles.add_style(
       font_name: font_name,
       fg_color: '000000',
       alignment: align.merge(wrap_text: true)
