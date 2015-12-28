301cbd5441bcbb18b259ffb1fb43926408b427b4
martent <marten@thavenius.se>
2015-12-28 16:58:44 +0100

    Refactoring reports generators to concerns

--- app/controllers/concerns/report_generator.rb
+++ app/controllers/concerns/report_generator.rb
@@ -2,47 +2,123 @@ module ReportGenerator
   extend ActiveSupport::Concern
 
   included do
-    def create_xlsx
-      @axlsx = Axlsx::Package.new
-      define_styles
+    def generate_xlsx(template_name, records)
+      @axlsx   = Axlsx::Package.new
+      template = Template.new(@axlsx)
+
+      template.send(template_name, records)
     end
 
-    def define_styles
-      font_name = 'Calibri'
-      align = { vertical: :top }
-
-      @heading_style = @axlsx.workbook.styles.add_style(
-        font_name: font_name,
-        bg_color: '000000',
-        fg_color: 'FFFFFF',
-        alignment: align,
-        width: 10
-      )
-      @cell_style = @axlsx.workbook.styles.add_style(
-        font_name: font_name,
-        fg_color: '000000',
-        alignment: align,
-      )
-      @wrap_style = @axlsx.workbook.styles.add_style(
-        font_name: font_name,
-        fg_color: '000000',
-        alignment: align.merge(wrap_text: true)
-      )
-      @date_format = @axlsx.workbook.styles.add_style(
-        font_name: font_name,
-        fg_color: '000000',
-        alignment: align,
-        format_code: 'yyyy-mm-dd'
-      )
+    def send_xlsx(stream, base_name)
+      send_data @axlsx.to_stream.read, type: :xlsx, disposition: "attachment",
+        filename: "#{base_name}_#{DateTime.now.strftime('%Y-%m-%d_%H%M%S')}.xlsx"
     end
 
-    def generate_filename(base)
-      "#{base}_#{DateTime.now.strftime('%Y-%m-%d_%H%M%S')}"
+    class Template
+      def initialize(axlsx)
+        @axlsx = axlsx
+        @style = Style.new(@axlsx)
+      end
+
+      def placements(records)
+        @axlsx.workbook.add_worksheet do |sheet|
+          sheet.add_row [
+            'Barn',
+            'Dossiernummer',
+            'Personnummer',
+            'Boende',
+            'Placerad',
+            'Utskriven',
+            'Total placeringstid (dagar)',
+            'Anledning till utskrivning',
+            'Kommentar till utskrivning'
+          ],
+          style: @style.heading
+
+          records.find_each do |placement|
+            sheet.add_row([
+              placement.refugee.name,
+              placement.refugee.dossier_numbers.map(&:name).join(', '),
+              placement.refugee.ssns.map(&:name).join(', '),
+              placement.home.name,
+              placement.moved_in_at,
+              placement.moved_out_at,
+              placement.placement_time,
+              placement.moved_out_reason.present? ? placement.moved_out_reason.name : '',
+              placement.comment
+            ],
+            style: [
+              @style.normal,
+              @style.normal,
+              @style.normal,
+              @style.normal,
+              @style.date,
+              @style.date,
+              @style.normal,
+              @style.normal,
+              @style.wrap
+            ],
+            types: [
+              :string,
+              :string,
+              :string,
+              :string,
+              :date,
+              :date,
+              :integer,
+              :string,
+              :string
+            ])
+          end
+          sheet.column_info.each { |c| c.width = 20 }
+          sheet.column_info[4].width = 12
+          sheet.column_info[5].width = 12
+          sheet.column_info[8].width = 40
+        end
+      end
     end
 
-    def send_xlsx(stream, name)
-      send_data stream, type: :xlsx, disposition: "attachment",
-        filename: "#{generate_filename(name)}.xlsx"
+    class Style
+      def initialize(axlsx)
+        @axlsx = axlsx
+        @font = 'Calibri'
+        @align_top = { vertical: :top }
+      end
+
+      def heading
+        @axlsx.workbook.styles.add_style(
+          font_name: @font,
+          bg_color: '000000',
+          fg_color: 'FFFFFF',
+          alignment: @align_top,
+          width: 10
+        )
+      end
+
+      def normal
+        @axlsx.workbook.styles.add_style(
+          font_name: @font,
+          fg_color: '000000',
+          alignment: @align_top
+        )
+      end
+
+      def wrap
+        @axlsx.workbook.styles.add_style(
+          font_name: @font,
+          fg_color: '000000',
+          alignment: @align_top.merge(wrap_text: true)
+        )
+      end
+
+      def date
+        @axlsx.workbook.styles.add_style(
+          font_name: @font,
+          fg_color: '000000',
+          alignment: @align_top,
+          format_code: 'yyyy-mm-dd'
+        )
+      end
     end
   end
 
--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,137 +1,83 @@
 class ReportsController < ApplicationController
   include ReportGenerator
 
-  before_action :create_xlsx, except: [:index]
-
   def index
   end
 
   def placements
-    @axlsx.workbook.add_worksheet do |sheet|
-      sheet.add_row [
-        'Barn',
-        'Dossiernummer',
-        'Personnummer',
-        'Boende',
-        'Placerad',
-        'Utskriven',
-        'Total placeringstid (dagar)',
-        'Anledning till utskrivning',
-        'Kommentar till utskrivning'
-      ],
-      style: @heading_style
+    records = Placement.includes(
+      :refugee, :home, :moved_out_reason, refugee: [:dossier_numbers, :ssns])
 
-      Placement.includes(
-        :refugee, :home, :moved_out_reason, refugee: [:dossier_numbers, :ssns]).find_each do |placement|
-        sheet.add_row([
-          placement.refugee.name,
-          placement.refugee.dossier_numbers.map(&:name).join(', '),
-          placement.refugee.ssns.map(&:name).join(', '),
-          placement.home.name,
-          placement.moved_in_at,
-          placement.moved_out_at,
-          placement.placement_time,
-          placement.moved_out_reason.present? ? placement.moved_out_reason.name : '',
-          placement.comment
-        ],
-        style: [
-          @cell_style,
-          @cell_style,
-          @cell_style,
-          @cell_style,
-          @date_format,
-          @date_format,
-          @cell_style,
-          @cell_style,
-          @wrap_style
-        ],
-        types: [
-          :string,
-          :string,
-          :string,
-          :string,
-          :date,
-          :date,
-          :integer,
-          :string,
-          :string
-        ])
-      end
-      sheet.column_info.each { |c| c.width = 20 }
-      sheet.column_info[4].width = 12
-      sheet.column_info[5].width = 12
-      sheet.column_info[8].width = 40
-    end
-    send_xlsx(@axlsx.to_stream.read, 'Placeringar')
+    send_xlsx generate_xlsx(:placements, records), 'Placeringar'
   end
 
-  def refugees
-    @axlsx.workbook.add_worksheet do |sheet|
-      sheet.add_row [
-        'Namn',
-        'Registrerad',
-        'Avregistrerad',
-        'Dossiernummer',
-        'Personnummer',
-        'Anvisad',
-        'Kön',
-        'Land',
-        'Språk',
-        'Aktuellt boenden',
-        'All boenden',
-        'Total placeringstid (dagar)'
-      ], style: @heading_style
-
-      Refugee.includes(
-          :countries, :languages, :ssns, :dossier_numbers, :gender, :homes, :placements).find_each do |refugee|
-        sheet.add_row([
-          refugee.name,
-          refugee.registered,
-          refugee.deregistered,
-          refugee.dossier_numbers.map(&:name).join(', '),
-          refugee.ssns.map(&:name).join(', '),
-          refugee.municipality.present? ? refugee.municipality.name : '',
-          refugee.gender.present? ? refugee.gender.name : '',
-          refugee.languages.map(&:name).join(', '),
-          refugee.countries.map(&:name).join(', '),
-          refugee.placements.where(moved_out_at: nil).map(&:home).map(&:name).join(', '),
-          refugee.homes.map(&:name).join(', '),
-          refugee.total_placement_time
-        ],
-        style: [
-          @cell_style,
-          @date_format,
-          @date_format,
-          @cell_style,
-          @cell_style,
-          @cell_style,
-          @cell_style,
-          @cell_style,
-          @cell_style,
-          @cell_style,
-          @wrap_style,
-          @cell_style
-        ],
-        types: [
-          :string,
-          :date,
-          :date,
-          :string,
-          :string,
-          :string,
-          :string,
-          :string,
-          :string,
-          :string,
-          :string,
-          :integer
-        ])
-        sheet.column_info.each { |c| c.width = 20 }
-        sheet.column_info[6].width = 8
-        sheet.column_info[10].width = 25
-        sheet.column_info[11].width = 12
-      end
-    end
-    send_xlsx(@axlsx.to_stream.read, 'Ensamkommand-barn')
-  end
+  # def refugees
+  #   @axlsx.workbook.add_worksheet do |sheet|
+  #     sheet.add_row [
+  #       'Namn',
+  #       'Registrerad',
+  #       'Avregistrerad',
+  #       'Dossiernummer',
+  #       'Personnummer',
+  #       'Anvisad',
+  #       'Kön',
+  #       'Land',
+  #       'Språk',
+  #       'Aktuellt boenden',
+  #       'All boenden',
+  #       'Total placeringstid (dagar)'
+  #     ], style: @style.heading
+  #
+  #     Refugee.includes(
+  #         :countries, :languages, :ssns, :dossier_numbers, :gender, :homes, :placements).find_each do |refugee|
+  #       sheet.add_row([
+  #         refugee.name,
+  #         refugee.registered,
+  #         refugee.deregistered,
+  #         refugee.dossier_numbers.map(&:name).join(', '),
+  #         refugee.ssns.map(&:name).join(', '),
+  #         refugee.municipality.present? ? refugee.municipality.name : '',
+  #         refugee.gender.present? ? refugee.gender.name : '',
+  #         refugee.languages.map(&:name).join(', '),
+  #         refugee.countries.map(&:name).join(', '),
+  #         refugee.placements.where(moved_out_at: nil).map(&:home).map(&:name).join(', '),
+  #         refugee.homes.map(&:name).join(', '),
+  #         refugee.total_placement_time
+  #       ],
+  #       style: [
+  #         @style.normal,
+  #         @style.date_format,
+  #         @style.date_format,
+  #         @style.normal,
+  #         @style.normal,
+  #         @style.normal,
+  #         @style.normal,
+  #         @style.normal,
+  #         @style.normal,
+  #         @style.normal,
+  #         @style.wrap,
+  #         @style.normal
+  #       ],
+  #       types: [
+  #         :string,
+  #         :date,
+  #         :date,
+  #         :string,
+  #         :string,
+  #         :string,
+  #         :string,
+  #         :string,
+  #         :string,
+  #         :string,
+  #         :string,
+  #         :integer
+  #       ])
+  #       sheet.column_info.each { |c| c.width = 20 }
+  #       sheet.column_info[6].width = 8
+  #       sheet.column_info[10].width = 25
+  #       sheet.column_info[11].width = 12
+  #     end
+  #   end
+  #   send_xlsx(@axlsx.to_stream.read, 'Ensamkommand-barn')
+  # end
 end
