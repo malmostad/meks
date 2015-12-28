3793c7464ca4c436908446dc3bcc77e701d69303
martent <marten@thavenius.se>
2015-12-28 17:31:45 +0100

    Refactoring reports generators to concerns

--- app/controllers/concerns/report_generator.rb
+++ app/controllers/concerns/report_generator.rb
@@ -76,6 +76,73 @@ module ReportGenerator
           sheet.column_info[8].width = 40
         end
       end
+
+      def refugees(records)
+        @axlsx.workbook.add_worksheet do |sheet|
+          sheet.add_row [
+            'Namn',
+            'Registrerad',
+            'Avregistrerad',
+            'Dossiernummer',
+            'Personnummer',
+            'Anvisad',
+            'Kön',
+            'Land',
+            'Språk',
+            'Aktuellt boenden',
+            'All boenden',
+            'Placeringstid (dagar)'
+          ], style: @style.heading
+
+          records.find_each do |refugee|
+            sheet.add_row([
+              refugee.name,
+              refugee.registered,
+              refugee.deregistered,
+              refugee.dossier_numbers.map(&:name).join(', '),
+              refugee.ssns.map(&:name).join(', '),
+              refugee.municipality.present? ? refugee.municipality.name : '',
+              refugee.gender.present? ? refugee.gender.name : '',
+              refugee.languages.map(&:name).join(', '),
+              refugee.countries.map(&:name).join(', '),
+              refugee.placements.where(moved_out_at: nil).map(&:home).map(&:name).join(', '),
+              refugee.homes.map(&:name).join(', '),
+              refugee.total_placement_time
+            ],
+            style: [
+              @style.normal,
+              @style.date,
+              @style.date,
+              @style.normal,
+              @style.normal,
+              @style.normal,
+              @style.normal,
+              @style.normal,
+              @style.normal,
+              @style.normal,
+              @style.wrap,
+              @style.normal
+            ],
+            types: [
+              :string,
+              :date,
+              :date,
+              :string,
+              :string,
+              :string,
+              :string,
+              :string,
+              :string,
+              :string,
+              :string,
+              :integer
+            ])
+            sheet.column_info.each { |c| c.width = 20 }
+            sheet.column_info[6].width = 8
+            sheet.column_info[10].width = 25
+          end
+        end
+      end
     end
 
     class Style
--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -11,73 +11,10 @@ class ReportsController < ApplicationController
     send_xlsx generate_xlsx(:placements, records), 'Placeringar'
   end
 
-  # def refugees
-  #   @axlsx.workbook.add_worksheet do |sheet|
-  #     sheet.add_row [
-  #       'Namn',
-  #       'Registrerad',
-  #       'Avregistrerad',
-  #       'Dossiernummer',
-  #       'Personnummer',
-  #       'Anvisad',
-  #       'Kön',
-  #       'Land',
-  #       'Språk',
-  #       'Aktuellt boenden',
-  #       'All boenden',
-  #       'Total placeringstid (dagar)'
-  #     ], style: @style.heading
-  #
-  #     Refugee.includes(
-  #         :countries, :languages, :ssns, :dossier_numbers, :gender, :homes, :placements).find_each do |refugee|
-  #       sheet.add_row([
-  #         refugee.name,
-  #         refugee.registered,
-  #         refugee.deregistered,
-  #         refugee.dossier_numbers.map(&:name).join(', '),
-  #         refugee.ssns.map(&:name).join(', '),
-  #         refugee.municipality.present? ? refugee.municipality.name : '',
-  #         refugee.gender.present? ? refugee.gender.name : '',
-  #         refugee.languages.map(&:name).join(', '),
-  #         refugee.countries.map(&:name).join(', '),
-  #         refugee.placements.where(moved_out_at: nil).map(&:home).map(&:name).join(', '),
-  #         refugee.homes.map(&:name).join(', '),
-  #         refugee.total_placement_time
-  #       ],
-  #       style: [
-  #         @style.normal,
-  #         @style.date_format,
-  #         @style.date_format,
-  #         @style.normal,
-  #         @style.normal,
-  #         @style.normal,
-  #         @style.normal,
-  #         @style.normal,
-  #         @style.normal,
-  #         @style.normal,
-  #         @style.wrap,
-  #         @style.normal
-  #       ],
-  #       types: [
-  #         :string,
-  #         :date,
-  #         :date,
-  #         :string,
-  #         :string,
-  #         :string,
-  #         :string,
-  #         :string,
-  #         :string,
-  #         :string,
-  #         :string,
-  #         :integer
-  #       ])
-  #       sheet.column_info.each { |c| c.width = 20 }
-  #       sheet.column_info[6].width = 8
-  #       sheet.column_info[10].width = 25
-  #       sheet.column_info[11].width = 12
-  #     end
-  #   end
-  #   send_xlsx(@axlsx.to_stream.read, 'Ensamkommand-barn')
-  # end
+  def refugees
+    records = Refugee.includes(
+        :countries, :languages, :ssns, :dossier_numbers, :gender, :homes, :placements)
+
+    send_xlsx generate_xlsx(:refugees, records), 'Ensamkommand-barn'
+  end
 end
