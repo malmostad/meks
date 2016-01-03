7f3dccc2bd67d80ea6bbd209502b0491987f09a7
martent <marten@thavenius.se>
2016-01-03 17:32:46 +0100

    Changed logic in overlapping placements. Only one placement needs to fall within the range and the selected home

--- app/models/placement.rb
+++ app/models/placement.rb
@@ -15,24 +15,24 @@ class Placement < ActiveRecord::Base
 
 
   def self.overlapping_by_refugee(options = {})
-    # Limit to date range?
-    range = begin
-      "A.moved_in_at BETWEEN '#{Date.parse(options[:placements_from])}' AND '#{Date.parse(options[:placements_end])}'"
-    rescue
-      'true'
-    end
+    # Within range
+    range_from = begin; Date.parse(options[:placements_from]).to_s; rescue; (Date.today - 10.years).to_s; end
+    range_to   = begin; Date.parse(options[:placements_to]).to_s; rescue; Date.today.to_s; end
 
-    # Only for one home?
-    home_id = options[:home_id].present? && options[:home_id].reject(&:empty?).present? ? "A.home_id = #{options[:home_id].first}" : 'true'
+    # Single home or all
+    select_home = options[:home_id].present? && options[:home_id].reject(&:empty?).present? ?
+        "(A.home_id = #{options[:home_id].first} or B.home_id = #{options[:home_id].first})" : 'true'
 
-    # Select overlapping placements per refugee
+    # Select overlapping placements per refugee, within range, for home
     records = find_by_sql("
       select A.* from placements A
       inner join placements B on
         (B.moved_in_at <= A.moved_out_at or A.moved_out_at is null)
-      and (B.moved_out_at >= A.moved_in_at or B.moved_out_at is null)
-      and #{range}
-      and #{home_id}
+        and (B.moved_out_at >= A.moved_in_at or B.moved_out_at is null)
+      and ((B.moved_in_at   between '#{range_from}' and '#{range_to}') or (A.moved_in_at between '#{range_from}' and '#{range_to}'))
+      and ((A.moved_out_at between '#{range_from}' and '#{range_to}') or A.moved_out_at is null)
+      and ((B.moved_out_at between '#{range_from}' and '#{range_to}') or B.moved_out_at is null)
+      and #{select_home}
       and A.refugee_id = B.refugee_id
       and A.id <> B.id
       order by A.refugee_id")
