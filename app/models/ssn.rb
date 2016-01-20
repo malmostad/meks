2c6780ccc52715a316efd19d8a9ceaf770bee578
martent <marten@thavenius.se>
2016-01-20 19:49:24 +0100

    Date validation

--- app/models/ssn.rb
+++ app/models/ssn.rb
@@ -1,6 +1,9 @@
 class Ssn < ActiveRecord::Base
   belongs_to :refugee
 
+  validates :date_of_birth, format: { with: /\d{4}\-\d{2}\-\d{2}/,
+      message: "Ogiltigt datumformat, måste vara yyyy-mm-dd" }
+
   def full_ssn
     date_of_birth.to_s.gsub('-', '') + '-' + extension.to_s
   end
