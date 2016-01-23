f1ece01dfce2873964383be2b7aaef3826c6653f
martent <marten@thavenius.se>
2016-01-23 16:06:27 +0100

    Stricter validation

--- app/models/ssn.rb
+++ app/models/ssn.rb
@@ -1,7 +1,7 @@
 class Ssn < ActiveRecord::Base
   belongs_to :refugee
 
-  validates :date_of_birth, format: { with: /\d{4}\-\d{2}\-\d{2}/,
+  validates :date_of_birth, format: { with: /\A\d{4}\-\d{2}\-\d{2}\z/,
       message: "Ogiltigt datumformat, måste vara yyyy-mm-dd" }
 
   def full_ssn
