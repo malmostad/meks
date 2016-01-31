259e97b8e16f21ea0544182a05b4e900ec59e97d
martent <marten@thavenius.se>
2016-01-31 15:10:53 +0100

    Avoid dup names

--- spec/views/countries/index.html.haml_spec.rb
+++ spec/views/countries/index.html.haml_spec.rb
@@ -7,13 +7,14 @@ RSpec.describe "countries/index", type: :view do
         :name => "Name"
       ),
       Country.create!(
-        :name => "Name"
+        :name => "Name 2"
       )
     ])
   end
 
   it "renders a list of countries" do
     render
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    assert_select "tr>td", :text => "Name", :count => 1
+    assert_select "tr>td", :text => "Name 2", :count => 1
   end
 end
