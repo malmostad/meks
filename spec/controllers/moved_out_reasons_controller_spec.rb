b6bf0fede1ce18f563f9f61539a5ea074e3d8439
martent <marten@thavenius.se>
2016-02-01 15:34:43 +0100

    Added moved_out_reasons controller test cases

---
+++ spec/controllers/moved_out_reasons_controller_spec.rb
@@ -0,0 +1,125 @@
+require 'rails_helper'
+
+RSpec.describe MovedOutReasonsController, type: :controller do
+  let(:valid_attributes) {
+    { name: 'Flyttat' }
+  }
+
+  let(:invalid_attributes) {
+    { name: nil }
+  }
+
+  describe "GET #index" do
+    it "assigns all moved_out_reasons as @moved_out_reasons" do
+      moved_out_reason = MovedOutReason.create! valid_attributes
+      get :index, {}, valid_session
+      expect(assigns(:moved_out_reasons)).to eq([moved_out_reason])
+    end
+  end
+
+  describe "GET #new" do
+    it "assigns a new moved_out_reason as @moved_out_reason" do
+      get :new, {}, valid_session
+      expect(assigns(:moved_out_reason)).to be_a_new(MovedOutReason)
+    end
+  end
+
+  describe "GET #edit" do
+    it "assigns the requested moved_out_reason as @moved_out_reason" do
+      moved_out_reason = MovedOutReason.create! valid_attributes
+      get :edit, {:id => moved_out_reason.to_param}, valid_session
+      expect(assigns(:moved_out_reason)).to eq(moved_out_reason)
+    end
+  end
+
+  describe "POST #create" do
+    context "with valid params" do
+      it "creates a new MovedOutReason" do
+        expect {
+          post :create, {:moved_out_reason => valid_attributes}, valid_session
+        }.to change(MovedOutReason, :count).by(1)
+      end
+
+      it "assigns a newly created moved_out_reason as @moved_out_reason" do
+        post :create, {:moved_out_reason => valid_attributes}, valid_session
+        expect(assigns(:moved_out_reason)).to be_a(MovedOutReason)
+        expect(assigns(:moved_out_reason)).to be_persisted
+      end
+
+      it "redirects to the created moved_out_reason" do
+        post :create, {:moved_out_reason => valid_attributes}, valid_session
+        expect(response).to redirect_to(moved_out_reasons_url)
+      end
+    end
+
+    context "with invalid params" do
+      it "assigns a newly created but unsaved moved_out_reason as @moved_out_reason" do
+        post :create, {:moved_out_reason => invalid_attributes}, valid_session
+        expect(assigns(:moved_out_reason)).to be_a_new(MovedOutReason)
+      end
+
+      it "re-renders the 'new' template" do
+        post :create, {:moved_out_reason => invalid_attributes}, valid_session
+        expect(response).to render_template("new")
+      end
+
+      it "too long name" do
+        post :create, {:moved_out_reason => { name: 'x' * 200 }}, valid_session
+        expect(response).to render_template("new")
+      end
+    end
+  end
+
+  describe "PUT #update" do
+    context "with valid params" do
+      it "updates the requested moved_out_reason" do
+        moved_out_reason = MovedOutReason.create! valid_attributes
+        new_name = "#{moved_out_reason.name} updated"
+        put :update, {:id => moved_out_reason.to_param, :moved_out_reason => { name: new_name } }, valid_session
+        moved_out_reason.reload
+        expect(moved_out_reason.name).to eq(new_name)
+      end
+
+      it "assigns the requested moved_out_reason as @moved_out_reason" do
+        moved_out_reason = MovedOutReason.create! valid_attributes
+        put :update, {:id => moved_out_reason.to_param, :moved_out_reason => valid_attributes}, valid_session
+        expect(assigns(:moved_out_reason)).to eq(moved_out_reason)
+      end
+
+      it "redirects to the moved_out_reason" do
+        moved_out_reason = MovedOutReason.create! valid_attributes
+        put :update, {:id => moved_out_reason.to_param, :moved_out_reason => valid_attributes}, valid_session
+        expect(response).to redirect_to(moved_out_reasons_url)
+      end
+    end
+
+    context "with invalid params" do
+      it "assigns the moved_out_reason as @moved_out_reason" do
+        moved_out_reason = MovedOutReason.create! valid_attributes
+        put :update, {:id => moved_out_reason.to_param, :moved_out_reason => invalid_attributes}, valid_session
+        expect(assigns(:moved_out_reason)).to eq(moved_out_reason)
+      end
+
+      it "re-renders the 'edit' template" do
+        moved_out_reason = MovedOutReason.create! valid_attributes
+        put :update, {:id => moved_out_reason.to_param, :moved_out_reason => invalid_attributes}, valid_session
+        expect(response).to render_template("edit")
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    it "destroys the requested moved_out_reason" do
+      moved_out_reason = MovedOutReason.create! valid_attributes
+      expect {
+        delete :destroy, {:id => moved_out_reason.to_param}, valid_session
+      }.to change(MovedOutReason, :count).by(-1)
+    end
+
+    it "redirects to the moved_out_reasons list" do
+      moved_out_reason = MovedOutReason.create! valid_attributes
+      delete :destroy, {:id => moved_out_reason.to_param}, valid_session
+      expect(response).to redirect_to(moved_out_reasons_url)
+    end
+  end
+end
