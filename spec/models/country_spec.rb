80c3b9f0d528d08c9e08ec0c422418dfca58cad7
martent <marten@thavenius.se>
2016-02-03 12:00:40 +0100

    Refactoring to use factories...

--- spec/models/country_spec.rb
+++ spec/models/country_spec.rb
@@ -2,29 +2,29 @@ require 'rails_helper'
 
 RSpec.describe Country, type: :model do
   it "should be adding one" do
-    expect { Country.create(name: "Sverige") }.to change(Country, :count).by(+1)
+    expect { create(:country) }.to change(Country, :count).by(+1)
   end
 
   describe 'validation' do
     it "should be valid" do
-      expect(Country.create(name: "Sverige")).to be_valid
+      expect(build(:country)).to be_valid
     end
 
     it "should require a name" do
-      expect(Country.new).not_to be_valid
+      expect(build(:country, name: nil)).not_to be_valid
     end
 
     it "should be valid with a not too long name" do
-      expect(Country.new name: "x" * 191).to be_valid
+      expect(build(:country, name: "x" * 191)).to be_valid
     end
 
     it "should not have a too long name" do
-      expect(Country.new name: "x" * 192).not_to be_valid
+      expect(build(:country, name: "x" * 192)).not_to be_valid
     end
 
     it "should have a unique name" do
-      Country.create(name: "Sverige")
-      expect(Country.new name: "Sverige").not_to be_valid
+      create(:country, name: "Sverige")
+      expect(build(:country, name: "Sverige")).not_to be_valid
     end
 
     it "... just checking transactional support" do
@@ -34,7 +34,7 @@ RSpec.describe Country, type: :model do
 
   describe 'destroy' do
     it "should destroy a record" do
-      expect { Country.create(name: "Sverige") }.to change(Country, :count).by(+1)
+      expect { create(:country, name: "Sverige") }.to change(Country, :count).by(+1)
       expect { Country.where(name: "Sverige").first.destroy }.to change(Country, :count).by(-1)
     end
   end
