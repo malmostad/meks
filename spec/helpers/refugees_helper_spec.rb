cc5367a9b90d75337a53ec9b69d344977a140e0a
martent <marten@thavenius.se>
2016-02-04 14:02:34 +0100

    Test cases for helper methods

--- spec/helpers/refugees_helper_spec.rb
+++ spec/helpers/refugees_helper_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the RefugeesHelper. For example:
-#
-# describe RefugeesHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe RefugeesHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    helper.title('foo')
+  end
+
+  it "h1 be inherited from the title" do
+    expect(helper.h1).to eq("foo")
+  end
+
+  it "page_title should be contactenated from title and title_suffix" do
+    expect(helper.page_title).to eq("foo - #{helper.title_suffix}")
+  end
 end
