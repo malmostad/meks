31e64cfba95807ead0a9ea86e0cb1e8e98b55920
martent <marten@thavenius.se>
2016-02-05 11:53:39 +0100

    Reverted Unicorn restart

--- config/deploy.rb
+++ config/deploy.rb
@@ -90,6 +90,6 @@ namespace :deploy do
   end
 
   before :starting, "deploy:are_you_sure", "deploy:check_revision"
-  after :published, "deploy:copy_vendor_statics"
-  after :finishing, "deploy:full_restart", "deploy:cleanup"
+  after :published, "deploy:copy_vendor_statics", "deploy:full_restart"
+  after :finishing, "deploy:cleanup"
 end
