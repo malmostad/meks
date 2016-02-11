c728e6ba800db64ae0499db35e257a4ef5b7e091
martent <marten@thavenius.se>
2016-02-11 15:46:36 +0100

    Added test case for Home

---
+++ spec/features/homes_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+RSpec.describe "Homes", type: :feature do
+  describe "admin role" do
+    before(:each) do
+      login_user(:admin)
+    end
+
+    scenario "adds a home" do
+      visit "/homes"
+      first(".btn-primary", text: "Lägg till").click
+      fill_in "home_name", with: "Foo"
+      click_button "Spara"
+
+      expect(current_path).to eq home_path(Home.last)
+      expect(page).to have_selector(".notice", text: "skapades")
+    end
+
+    scenario "updates a home" do
+      home = create(:home, name: "Foo")
+      visit "/homes"
+      click_link home.name
+      click_link "Redigera"
+      fill_in "home_name", with: "Bar"
+      click_button "Spara"
+
+      expect(current_path).to eq home_path(home)
+      expect(page).to have_selector(".notice", text: "uppdaterades")
+      expect(page).to have_selector(".controls", text: "Bar")
+    end
+  end
+
+  describe "reader role" do
+    before(:each) do
+      login_user(:reader)
+    end
+
+    scenario "can't add a home" do
+      visit "/homes/new"
+
+      expect(current_path).to eq root_path
+      expect(page).to have_selector(".alert", text: "Din roll saknar behörighet")
+    end
+  end
+
+  describe "writer role" do
+    before(:each) do
+      login_user(:writer)
+    end
+
+    scenario "adds a home" do
+      visit "/homes"
+      first(".btn-primary", text: "Lägg till").click
+      fill_in "home_name", with: "Foo"
+      click_button "Spara"
+
+      expect(current_path).to eq home_path(Home.last)
+      expect(page).to have_selector(".notice", text: "skapades")
+    end
+  end
+end
