a8b7e4a0487e05a58003efd5625f89e579d01484
martent <marten@thavenius.se>
2016-02-16 09:36:05 +0100

    Nullify dependent

--- app/models/moved_out_reason.rb
+++ app/models/moved_out_reason.rb
@@ -1,5 +1,5 @@
 class MovedOutReason < ActiveRecord::Base
-  has_many :placements
+  has_many :placements, dependent: :nullify
 
   validates_uniqueness_of :name, case_sensitive: false
   validates_presence_of :name
