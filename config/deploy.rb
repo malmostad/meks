316929afa5290b9cd1cf1876e28db11ee6020734
martent <marten@thavenius.se>
2016-03-09 14:39:34 +0100

    Added reports dir to linked dir during deployments

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :pty, true
 set :forward_agent, true
 
 set :linked_files, %w{config/database.yml config/secrets.yml }
-set :linked_dirs, %w{log tmp/pids tmp/sockets}
+set :linked_dirs, %w{log tmp/pids tmp/sockets reports}
 
 set :default_env, { path: '$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH' }
 set :keep_releases, 5
