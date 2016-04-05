9cbce218221ef367ceddb3d17d9668d96790bac2
martent <marten@thavenius.se>
2016-04-05 15:09:33 +0200

    Prepare for session timeout

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, {
-  key: '_meks_session',
-  secure: Rails.env.production? || Rails.env.test?,
-  httponly: true
-}
+Rails.application.config.session_store :cookie_store,
+    key:      '_meks_session',
+    secure:   Rails.env.production? || Rails.env.test?,
+    httponly: true # , expire_after: 30.minutes
