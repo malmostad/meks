e8b8be21b0ae7f7087aa013d6d5fc6ada718d6fc
martent <marten@thavenius.se>
2016-05-25 13:01:38 +0200

    Routing specs, to warm up

---
+++ spec/routing/login_routing_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe LoginController, type: :routing do
+  describe "routing" do
+    it "routes to #new" do
+      expect(:get => "/login").to route_to("login#new")
+    end
+
+    it "routes to #create" do
+      expect(:post => "/login").to route_to("login#create")
+    end
+
+    it "routes to #destroy" do
+      expect(:get => "/logout").to route_to("login#destroy")
+    end
+  end
+end
---
+++ spec/routing/saml_routing_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.describe SamlController, type: :routing do
+  describe "routing" do
+    it "routes to #sso" do
+      expect(:get => "/saml/sso").to route_to("saml#sso")
+    end
+
+    it "routes to #consume" do
+      expect(:post => "/saml/consume").to route_to("saml#consume")
+    end
+
+    it "routes to #metadata" do
+      expect(:get => "/saml/metadata").to route_to("saml#metadata")
+    end
+
+    it "routes to #logout" do
+      expect(:get => "/saml/logout").to route_to("saml#logout")
+    end
+  end
+end
