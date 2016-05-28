221033bafdfbf3afdb17f7f27c33ee3a5c7223a2
martent <marten@thavenius.se>
2016-05-28 15:16:15 +0200

    Added nullify on refugee for citizenship

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,5 +1,8 @@
 class Country < ActiveRecord::Base
   has_and_belongs_to_many :refugees
+  has_many :citizenships,
+           class_name: 'Refugee',
+           foreign_key: :citizenship_id, dependent: :nullify
 
   validates_uniqueness_of :name, case_sensitive: false
   validates_presence_of :name
