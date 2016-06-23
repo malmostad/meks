f94067757935a773f08f1c6759562144a7955fa7
martent <marten@thavenius.se>
2016-06-23 10:33:06 +0200

    Changed extension type to string on Ssn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160622133106) do
+ActiveRecord::Schema.define(version: 20160623082955) do
 
   create_table "countries", force: :cascade do |t|
     t.string   "name",       limit: 191
@@ -212,10 +212,10 @@ ActiveRecord::Schema.define(version: 20160622133106) do
 
   create_table "ssns", force: :cascade do |t|
     t.date     "date_of_birth"
-    t.integer  "extension",     limit: 4
+    t.string   "extension",     limit: 191
     t.integer  "refugee_id",    limit: 4
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
   add_index "ssns", ["refugee_id"], name: "index_ssns_on_refugee_id", using: :btree
