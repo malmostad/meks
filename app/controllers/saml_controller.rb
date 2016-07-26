272e33b6ad6af806d0af5d13dedc791cdc4136c0
martent <marten@thavenius.se>
2016-07-26 17:12:56 +0200

    Pem based saml

--- app/controllers/saml_controller.rb
+++ app/controllers/saml_controller.rb
@@ -81,9 +81,15 @@ class SamlController < ApplicationController
     settings.issuer                         = base_url
 
     # Non-metadata URI settings
-    # settings.idp_cert                       = @config['idp_cert']
+    settings.idp_cert                       = @config['idp_cert']
 
-    settings.idp_cert_fingerprint           = @config['idp_cert_fingerprint']
+    # settings.idp_cert_fingerprint           = @config['idp_cert_fingerprint']
+    # settings.idp_cert_fingerprint_algorithm = 'http://www.w3.org/2000/09/xmldsig#sha1'
+    settings.name_identifier_format         = "urn:oasis:names:tc:SAML:2.0:nameid-format:emailAddress"
+    settings.authn_context                  = "urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport"
+    settings.compress_request               = false
+
+    # settings.idp_cert_fingerprint           = @config['idp_cert_fingerprint']
     # settings.idp_cert_fingerprint_algorithm = 'http://www.w3.org/2000/09/xmldsig#sha1'
     # settings.name_identifier_format         = 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress'
 
