241e4e66ec04db5acdcf802cc54a9664f2fc4cda
martent <marten@thavenius.se>
2016-10-24 15:10:06 +0200

    Moved report generation queries from controller to lib

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,98 +1,46 @@
 class ReportsController < ApplicationController
+  # FIXME: set auth correctly in Ability
   skip_authorize_resource
   skip_authorization_check
   before_action { authorize! :generate, :reports }
-
-  include ReportGenerator
 
   def index
     @homes = Home.order(:name)
     @pre_generated_reports = pre_generated_reports
   end
 
-  def downloads
-    filename = find_report(params[:id].to_i)['filename']
-    send_file report_filepath(filename), type: :xlsx, disposition: 'attachment', filename: filename
-  end
-
   def placements
-    records = Placement.includes(
-      :refugee, :home, :moved_out_reason,
-      refugee: [:countries, :languages, :ssns, :dossier_numbers,
-                :gender, :homes, :placements, :municipality,
-                :relateds, :inverse_relateds, :deregistered_reason],
-      home: [:languages, :type_of_housings, :placements,
-             :owner_type, :target_groups, :languages]
-    ).within_range(params[:placements_from], params[:placements_to])
-
-    # Selected one home or all
-    if params[:placements_home_id].present? && params[:placements_home_id].reject(&:empty?).present?
-      records = records.where(home_id: params[:placements_home_id])
-    end
-
-    # Only overlapping placements in time per refugee
-    if params[:placements_selection] == 'overlapping'
-      records = Placement.overlapping_by_refugee(params)
-    end
-
-    xlsx = generate_xlsx(:placements, records)
-    send_xlsx xlsx, 'Placeringar'
+    queue_job('placements', params)
+    render plain: "Job id: #{@delayed_job_id} File id: #{@file_id}"
   end
 
   def refugees
-    records = Refugee.includes(
-      :countries, :languages, :ssns, :dossier_numbers,
-      :gender, :homes, :municipality, :deregistered_reason,
-      relationships: [:type_of_relationship, :refugee],
-      inverse_relationships: [:type_of_relationship, :refugee],
-      current_placements: [home: :type_of_housings])
-
-    if params[:refugees_registered_from].present? && params[:refugees_registered_to].present?
-      records = records.where(registered: params[:refugees_registered_from]..params[:refugees_registered_to])
-    end
-
-    query = [(params[:refugees_born_after]..params[:refugees_born_before])]
-    query << nil if params[:refugees_include_without_date_of_birth]
-    if params[:refugees_born_after].present? && params[:refugees_born_before].present?
-      records = records.where(date_of_birth: query)
-    end
-
-    if params[:refugees_asylum].present?
-      query = []
-      if params[:refugees_asylum].include? 'put'
-        query << 'refugees.residence_permit_at is not null'
-      end
-      if params[:refugees_asylum].include? 'tut'
-        query << 'refugees.temporary_permit_starts_at is not null'
-      end
-      if params[:refugees_asylum].include? 'municipality'
-        query << 'refugees.municipality_id is not null'
-      end
-      records = records.where(query.join(' or '))
-    end
-
-    xlsx = generate_xlsx(:refugees, records)
-    send_xlsx xlsx, 'Ensamkommand-barn'
+    queue_job('refugees', params)
+    render plain: "Job id: #{@delayed_job_id} File id: #{@file_id}"
   end
 
   def homes
-    records = Home.includes(
-      :placements, :type_of_housings,
-      :owner_type, :target_groups, :languages)
+    queue_job('homes', params)
+    render plain: "Job id: #{@delayed_job_id} File id: #{@file_id}"
+  end
 
-    if params[:homes_owner_type].present?
-      records = records.where(owner_type: params[:homes_owner_type])
-    end
+  def status(job_id)
+    job_id
+  end
 
-    if params[:homes_free_seats] == 'with'
-      records = records.each.reject { |r| r.free_seats <= 0 }
-    end
-
-    xlsx = generate_xlsx(:homes, records)
-    send_xlsx xlsx, 'Boenden'
+  def downloads
+    filename = find_report(params[:id].to_i)
+    send_file report_filepath(filename), type: :xlsx, disposition: 'attachment', filename: filename
   end
 
   private
+
+  def queue_job(report_type, params)
+    @file_id = "#{@current_user.username}_#{Time.now.to_f}".delete('.')
+
+    job = GenerateReportJob.perform_later(report_type, params, @file_id)
+    @delayed_job_id = Delayed::Job.find(job.provider_job_id).id
+  end
 
   def pre_generated_reports
     APP_CONFIG['pre_generated_reports'].each do |report|
@@ -115,7 +63,8 @@ class ReportsController < ApplicationController
     APP_CONFIG['pre_generated_reports'].detect { |r| r['id'] == id.to_i }
   end
 
-  def numshort_date(date)
-    I18n.l(date, format: :numshort) unless date.nil?
+  def send_xlsx(xlsx, base_name)
+    send_data xlsx.to_stream.read, type: :xlsx, disposition: 'attachment',
+      filename: "#{base_name}.xlsx"
   end
 end
