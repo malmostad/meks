5aac0bf8ef8fd53324c468fb36d9fbeb946f6f0d
martent <marten@thavenius.se>
2016-10-31 13:31:12 +0100

    Specs for report generation and queued jobs

---
+++ spec/features/reports_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe "Reports", type: :feature do
+  describe "admin role" do
+    before(:each) do
+      login_user(:admin)
+    end
+
+    scenario "queue a report" do
+      visit "/reports"
+      first(:button, "Skapa rapport").click
+      expect(page).to have_selector("p", text: "Det kan ta flera minuter att skapa din rapport")
+    end
+  end
+
+  describe "reader role" do
+    before(:each) do
+      login_user(:reader)
+    end
+
+    scenario "can't access reports" do
+      visit "/reports"
+      expect(current_path).to eq root_path
+      expect(page).to have_selector(".alert", text: "Din roll saknar behörighet")
+    end
+  end
+
+  describe "writer role" do
+    before(:each) do
+      login_user(:writer)
+    end
+
+    scenario "can access reports" do
+      visit "/reports"
+
+      expect(current_path).to eq reports_path
+      expect(page).to have_selector("h1", text: "Färdiggenererade rapporter")
+    end
+  end
+end
