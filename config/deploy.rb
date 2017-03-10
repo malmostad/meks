d625a8b8c98c2db2cc82f3116accf9eaca445811
martent <marten@thavenius.se>
2017-03-10 10:56:56 +0100

    Fixed rb version

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ require 'erb'
 I18n.config.enforce_available_locales = false
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.3.0'
+set :rbenv_ruby, '2.3.1'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 
