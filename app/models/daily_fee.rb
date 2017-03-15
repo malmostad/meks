7fdb30e5c2c30ceb423bf45112605ab6af4d10bb
martent <marten@thavenius.se>
2017-03-15 18:08:19 +0100

    Order scope

--- app/models/daily_fee.rb
+++ app/models/daily_fee.rb
@@ -2,6 +2,8 @@
 class DailyFee < ApplicationRecord
   belongs_to :home
 
+  default_scope { order(:start_date) }
+
   validates :home, :fee, :start_date, presence: true
   validates :fee, numericality: true
   validates :start_date, format: { with: /\A\d{4}\-\d{2}\-\d{2}\z/,
