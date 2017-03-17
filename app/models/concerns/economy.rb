121ae4684ac359be33c5a3f9779be842c7fa604f
martent <marten@thavenius.se>
2017-03-17 11:50:08 +0100

    Default arg value

--- app/models/concerns/economy.rb
+++ app/models/concerns/economy.rb
@@ -2,11 +2,11 @@ module Economy
   extend ActiveSupport::Concern
 
   module ClassMethods
-    def age_0_to_17(at_date)
+    def age_0_to_17(at_date = Date.today)
       where('date_of_birth <= ? and date_of_birth > ?', at_date - 0.years.ago.to_date, at_date - 18.years.ago.to_date)
     end
 
-    def age_18_to_20(at_date)
+    def age_18_to_20(at_date = Date.today)
       where('date_of_birth <= ? and date_of_birth > ?', at_date - 18.years.ago.to_date, at_date - 20.years.ago.to_date)
     end
   end
