27b48ba9e5ef5f046d77e9bb24f1a7b436489ac7
martent <marten@thavenius.se>
2017-04-12 14:42:04 +0200

    Reworking stats overview

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -4,106 +4,55 @@ class StatisticsController < ApplicationController
   before_action { authorize! :view, :statistics }
 
   def index
-    data = [['X', 'antal']]
-    registered_per_day(7.day.ago.to_date..1.day.ago.to_date).each do |date, quantity|
-      data << [I18n.l(date, format: :short), quantity]
+    @refugees = Rails.cache.fetch("queries-#{cache_key_for_status}") do
+      [
+        "#{registered.count} antal barn är i ankomst i väntan på anvisning",
+        "Malmö SRF ansvarar för #{refugees_srf.count} antal barn, varav #{refugees_srf_men.count} är pojkar och #{refugees_srf_women.count} är flickor",
+        "Vanligaste nationaliteterna (10 barn eller fler, visa de 3 vanligaste nationaliteterna)",
+        "X antal är asylsökande och väntar på beslut i asylärendet",
+        "X antal har PUT",
+        "X antal har TUT",
+        "X antal har erhållit svenskt medborgarskap"
+      ]
     end
 
-    @chart = {
-      id: 'weekly',
-      title: 'Antal nyanlända barn senaste sju dagarna',
-      data: data
-    }
-
-    @stats = Rails.cache.fetch("queries-#{cache_key_for_status}") do
-      {
-        periods: [
-          { id: 'all',
-            title: 'Aktuella ärenden just nu',
-            data: stats_for_collection(all_registered),
-          },
-          { id: 'this-month',
-            title: 'Nyinskrivna aktuell månad',
-            data: stats_for_collection(registered_this_month)
-          },
-          { id: 'this-year',
-            title: 'Nyinskrivna detta år',
-            data: stats_for_collection(registered_this_year) }
-        ],
-
-        homes: Home.where(active: true).count,
-        home_by_owner_type: OwnerType.all.map { |ot|
-          "#{Home.where(active: true, owner_type: ot).count} är #{ot.name}" }.join(', '),
-
-        current_placed_refugees: Placement.where(moved_out_at: nil).select(:refugee_id).distinct,
-        current_placements: Placement.current_placements,
-        current_placements_per_owner_type: OwnerType.all.map { |ot|
-          "#{Placement.current_placements.includes(:home).where(
-            homes: { active: true, owner_type_id: ot.id }).count} på #{ot.name}" }.join(', '),
-
-        guaranteed_seats: Home.where(active: true).sum(:guaranteed_seats),
-        movable_seats: Home.where(active: true).sum(:movable_seats),
-      }
+    @placements_homes = Rails.cache.fetch("queries-#{cache_key_for_status}") do
+      [
+        "X antal barn är placerade på kommunala HVB",
+        "X antal barn är externt placerade",
+        "X antal barn är placerade i jourhem",
+        "X antal barn är placerade i familjehem",
+        "X antal barn är placerade på utslussboende",
+        "X antal barn tillhörande Malmös stadsområden är placerade på SRF:s boenden",
+        "Det finns X boenden med X antal boendeplatser. På samtliga boenden finns det totalt X antal tomma platser"
+      ]
     end
   end
 
   private
 
-  def stats_for_collection(collection)
-    malmo_srf = collection.joins(:municipality).where("municipalities.name like ?", "malmö%srf").count
-    malmo_total = collection.joins(:municipality).where("municipalities.name like ?", "malmö%").count
-    {
-      refugees: collection.count,
-      per_gender: Gender.all.map { |g| "#{collection.where(gender: g).count} är #{g.name.downcase}" }.join(', '),
-      in_transit: collection.where(
-          residence_permit_at: nil,
-          temporary_permit_starts_at: nil,
-          municipality: nil,
-          municipality_placement_migrationsverket_at: nil,
-          municipality_placement_per_agreement_at: nil,
-          deregistered: nil).count,
-      with_residence_permit: collection.where.not(residence_permit_at: nil).count,
-      with_municipality_placement_in_malmo_srf: malmo_srf,
-      with_municipality_placement_in_malmo_sdo: malmo_total - malmo_srf,
-      with_municipality_placement_others: collection.where.not(municipality: nil).count - malmo_total,
-      with_no_municipality_placement: collection.includes(:placements).where(municipality: nil, deregistered_reason: nil, deregistered: nil).where.not(placements: { id: nil } ).count,
-      with_municipality_placement_and_not_deregistered: collection.where.not(municipality_id: nil).where(deregistered: nil).count,
-      with_municipality_placement_in_malmo_and_deregistered: collection.joins(:municipality).where('municipalities.name not like ? and deregistered is not ?', "malmö%", nil).count,
-      at_external_home: Placement.includes(:home).where(refugee: collection, moved_out_at: nil, homes: { owner_type_id: 3 }).select(:refugee_id).distinct.count,
-      top_countries: collection.joins(:countries).select('countries.name').group('countries.name').count('countries.name').sort_by{ |key, value| value }.reject { |k, v| v <= 10  }.reverse
-    }
+  # "I ankomst i väntan på anvisning"
+  def registered
+    Refugee.where.not(registered: nil)
+      .where(deregistered: nil)
+      .where(temporary_permit_starts_at: nil)
+      .where(residence_permit_at: nil)
+      .where(sof_placement: false)
+      .where.not(municipality_id: 135) # 135 is hardcoded "Malmö kommun, Srf"
   end
 
-  def all_registered
-    @all_registered ||= Refugee.all
+  # "Malmö SRF ansvarar för X antal barn"
+  def refugees_srf
+    Refugee.where.not(municipality_id: 135)
+      .where(deregistered: nil) # 135 is hardcoded "Malmö kommun, Srf"
   end
 
-  def registered_this_year
-    @registered_this_year ||=
-      Refugee.where('registered >= ?', Date.today.beginning_of_year)
+  def refugees_srf_women
+    refugees_srf.where(gender_id: 1) # 1 is hardcoded women
   end
 
-  def registered_this_quarter
-    @registered_this_quarter ||=
-      Refugee.where('registered >= ?', Date.today.beginning_of_quarter)
-  end
-
-  def registered_this_month
-    @registered_this_month ||=
-      Refugee.where('registered >= ?', Date.today.beginning_of_month)
-  end
-
-  def registered_last_week
-    @registered_last_week ||=
-      one_week_ago = Date.today - 1.week
-      Refugee.where(registered: one_week_ago.beginning_of_week..one_week_ago.end_of_week)
-  end
-
-  def registered_per_day(range)
-    zero_included = {}
-    range.each { |d| zero_included.merge!(d => 0)}
-    registered = Refugee.where(registered: range).select('registered').group('registered').count('registered')
-    zero_included.merge(registered)
+  def refugees_srf_men
+    refugees_srf.where(gender_id: 2) # 2 is hardcoded men
   end
 
   def cache_key_for_status
