056a6d67d32e352fc86f704c97ac994595738a31
martent <marten@thavenius.se>
2017-04-21 15:36:14 +0200

    Record as argument

--- lib/report_generator/workbooks/homes.rb
+++ lib/report_generator/workbooks/homes.rb
@@ -1,104 +1,113 @@
 class ReportGenerator::Workbooks
-  def self.homes
-    [
-      {
-        heading: "Foo",
-        # query: todo,
-        formula: true
-      },
-      {
-        heading: "Bar",
-        # query: todo,
-        formula: true
-      },
-      {
-        heading: "Form",
-        # query: todo,
-        formula: true
-      },
-      {
-        heading: "name",
-        # query: record.name
-      },
-      {
-        heading: "phone",
-        # query: record.phone
-      },
-      {
-        heading: "fax",
-        # query: record.fax
-      },
-      {
-        heading: "address",
-        # query: record.address
-      },
-      {
-        heading: "post_code",
-        # query: record.post_code
-      },
-      {
-        heading: "postal_town",
-        # query: record.postal_town
-      },
-      {
-        heading: "type_of_housings",
-        # query: record.type_of_housings.map(&:name).join(', ')
-      },
-      {
-        heading: "owner_type",
-        # query: record.owner_type.name
-      },
-      {
-        heading: "target_groups",
-        # query: record.target_groups.map(&:name).join(', ')
-      },
-      {
-        heading: "languages",
-        # query: record.languages.map(&:name).join(', ')
-      },
-      {
-        heading: "comment",
-        # query: record.comment
-      },
-      {
-        heading: "Aktuella placeringar",
-        # query: record.placements.reject { |p| !p.moved_out_at.nil?  }.size,
-        type: :integer
-      },
-      {
-        heading: "Placeringar totalt",
-        # query: record.placements.count,
-        type: :integer
-      },
-      {
-        heading: "Total placeringstid (dagar)",
-        # query: record.total_placement_time,
-        type: :integer
-      },
-      {
-        heading: "guaranteed_seats",
-        # query: record.guaranteed_seats,
-        type: :integer
-      },
-      {
-        heading: "Lediga platser",
-        # query: (record.guaranteed_seats + record.movable_seats) - record.placements.reject { |p| !p.moved_out_at.nil?  }.size,
-        type: :integer
-      },
-      {
-        heading: "movable_seats",
-        # query: record.movable_seats,
-        type: :integer
-      },
-      {
-        heading: "Summa platser",
-        # query: record.seats,
-        type: :integer
-      },
-      {
-        heading: "active",
-        # query: record.active? ? 'Ja' : 'Nej'
-      }
-    ]
+  class Homes
+
+    def initialize(home)
+      @home = home
+    end
+
+    def headings
+    end
+
+    def queries
+    end
+
+    def styles
+    end
+
+    def tooltips
+    end
+
+    def formats
+    end
+
+    private
+
+    def homes_data
+      [
+        {
+          heading: "name",
+          query: @home.name
+        },
+        {
+          heading: "phone",
+          query: @home.phone
+        },
+        {
+          heading: "fax",
+          query: @home.fax
+        },
+        {
+          heading: "address",
+          query: @home.address
+        },
+        {
+          heading: "post_code",
+          query: @home.post_code
+        },
+        {
+          heading: "postal_town",
+          query: @home.postal_town
+        },
+        {
+          heading: "type_of_housings",
+          query: @home.type_of_housings.map(&:name).join(', ')
+        },
+        {
+          heading: "owner_type",
+          query: @home.owner_type.name
+        },
+        {
+          heading: "target_groups",
+          query: @home.target_groups.map(&:name).join(', ')
+        },
+        {
+          heading: "languages",
+          query: @home.languages.map(&:name).join(', ')
+        },
+        {
+          heading: "comment",
+          query: @home.comment
+        },
+        {
+          heading: "Aktuella placeringar",
+          query: @home.placements.reject { |p| !p.moved_out_at.nil?  }.size,
+          type: :integer
+        },
+        {
+          heading: "Placeringar totalt",
+          query: @home.placements.count,
+          type: :integer
+        },
+        {
+          heading: "Total placeringstid (dagar)",
+          query: @home.total_placement_time,
+          type: :integer
+        },
+        {
+          heading: "guaranteed_seats",
+          query: @home.guaranteed_seats,
+          type: :integer
+        },
+        {
+          heading: "Lediga platser",
+          query: (@home.guaranteed_seats + @home.movable_seats) - @home.placements.reject { |p| !p.moved_out_at.nil?  }.size,
+          type: :integer
+        },
+        {
+          heading: "movable_seats",
+          query: @home.movable_seats,
+          type: :integer
+        },
+        {
+          heading: "Summa platser",
+          query: @home.seats,
+          type: :integer
+        },
+        {
+          heading: "active",
+          query: @home.active? ? 'Ja' : 'Nej'
+        }
+      ]
+    end
   end
 end
