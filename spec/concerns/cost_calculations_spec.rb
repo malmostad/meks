244e466c0516b62e4ac151623f7dcc967b02089f
martent <marten@thavenius.se>
2017-05-05 16:32:08 +0200

    Spec file

---
+++ spec/concerns/cost_calculations_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'Cost calculation' do
+  let(:cost) { create(:cost) }
+
+  xit "should get the number of days from a placement" do
+  end
+
+  xit "should get the number of days from a placement within a report range" do
+  end
+
+  describe 'for home costs' do
+    xit "should get the daily home cost from a placement" do
+    end
+  end
+
+  describe 'for placements cost' do
+    xit "should get the daily cost for a placement" do
+    end
+  end
+
+  xit "should get the costs for multiple placements" do
+  end
+
+  xit "should get the costs for a placements with serveral costs" do
+  end
+end
