4aa7e6d61d2395415d86a53a43245930bbef6af9
martent <marten@thavenius.se>
2017-05-15 18:18:32 +0200

    default sort order

--- app/models/gender.rb
+++ app/models/gender.rb
@@ -1,6 +1,8 @@
 class Gender < ApplicationRecord
   has_many :refugees, dependent: :nullify
 
+  default_scope { order(:name) }
+
   validates_uniqueness_of :name, case_sensitive: false
   validates_presence_of :name
   validates_length_of :name, maximum: 191
