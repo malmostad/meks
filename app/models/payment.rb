29e1ecfc1bb94e7f35c2431f6dee6fd1f38d9a57
martent <marten@thavenius.se>
2017-05-19 12:05:02 +0200

    Assoc changes

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,5 +2,5 @@ class Payment < ApplicationRecord
   belongs_to :refugee
   belongs_to :payment_import
 
-  validates_presence_of :refugee_id, :period_start, :period_end, :amount, :payment_import_id
+  validates_presence_of :refugee_id, :period_start, :period_end, :amount
 end
--- app/models/payment_import.rb
+++ app/models/payment_import.rb
@@ -1,6 +1,11 @@
 class PaymentImport < ApplicationRecord
   belongs_to :user
+
   has_many :payments, dependent: :destroy
+  accepts_nested_attributes_for :payments, allow_destroy: true
+  validates_associated :payments
 
   validates_presence_of :user_id, :imported_at, :number_of_records
+
+  has_many :rates, dependent: :destroy
 end
