3eb0fc6b333f1a630a43d03e6de4e9ac34ffd539
martent <marten@thavenius.se>
2017-06-29 16:01:29 +0200

    Fixed n+1

--- app/models/concerns/refugee_selections.rb
+++ app/models/concerns/refugee_selections.rb
@@ -1,10 +1,9 @@
 module RefugeeSelections
   extend ActiveSupport::Concern
-  DEFAULT_RANGE = { from: Date.new(0), to: Date.today }.freeze
 
   module ClassMethods
     def per_type_of_housing(type_of_housing, registered = DEFAULT_RANGE)
-      includes(:payments, placements: { home: :type_of_housings })
+      includes(:payments, placements: { home: [:type_of_housings, :costs] })
         .where(placements: { home: { type_of_housings: { id: type_of_housing.id } } })
         .where(registered: registered[:from]..registered[:to])
     end
