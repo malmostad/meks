3b5d90bbe26e89a26b3181ec963b273eb9dadf9f
martent <marten@thavenius.se>
2017-07-04 12:07:07 +0200

    Fixed cache key

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -9,13 +9,13 @@ class StatisticsController < ApplicationController
   private
 
   def cache_key_for_status
-    @cache_key_for_status ||=
-      count = Refugee.count + Home.count + Placement.count
-      latest_update = [
-        Refugee.maximum(:updated_at),
-        Home.maximum(:updated_at),
-        Setting.maximum(:updated_at),
-        Placement.maximum(:updated_at)].reject(&:blank?).sort.last.try(:utc).try(:to_s, :number)
-      "status/index-#{Date.today.to_s}-#{count}-#{latest_update}"
+    count = Refugee.count + Home.count + Placement.count + Setting.count
+    latest_update = [
+      Refugee.maximum(:updated_at),
+      Home.maximum(:updated_at),
+      Setting.maximum(:updated_at),
+      Placement.maximum(:updated_at)
+    ].reject(&:blank?).sort.last.try(:utc).try(:to_s, :number)
+    "status/index-#{Date.today}-#{count}-#{latest_update}"
   end
 end
