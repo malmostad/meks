c9cd34181c4be79bd038f0dcff44b0af4c42008b
martent <marten@thavenius.se>
2017-08-15 15:05:17 +0200

    refactoring

--- app/jobs/generate_report_job/economy.rb
+++ app/jobs/generate_report_job/economy.rb
@@ -3,28 +3,23 @@ class GenerateReportJob
     queue_as :default
 
     def perform(params, file_id)
+      @from = params[:placements_from]
+      @to = params[:placements_to]
       filename = "#{file_id}.xlsx"
       refugees = refugees_query(params)
-      create_workbook(refugees, params, filename)
+      create_workbook(refugees, filename)
     end
 
     private
 
-    def create_workbook(refugees, params, filename)
-      from = params[:placements_from]
-      to = params[:placements_to]
-      workbook = Report::Workbooks::Economy.new(from: from, to: to)
-      report = Report::Generator.new(workbook, refugees, from: from, to: to)
+    def create_workbook(refugees, filename)
+      workbook = Report::Workbooks::Economy.new(from: @from, to: @to)
+      report = Report::Generator.new(workbook, refugees, from: @from, to: @to)
       report.to_xlsx(filename)
     end
 
-    def refugees_query(params)
-      Refugee.includes(
-        :dossier_numbers, :ssns,
-        :municipality,
-        :gender, :homes, :municipality,
-        current_placements: [home: :type_of_housings]
-      ).where(registered: params[:placements_from]..params[:placements_to])
+    def refugees_query
+      Refugee.includes(placements: { home: :type_of_housings }).with_placements_within(@from, @to)
     end
   end
 end
