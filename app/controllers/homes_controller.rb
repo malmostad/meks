1e64980dfb086bd209b62f88c87969931bcea030
martent <marten@thavenius.se>
2017-08-30 14:15:39 +0200

    Removed default scope for Placement

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -9,8 +9,7 @@ class HomesController < ApplicationController
 
   def show
     @home = Home.includes(:owner_type, :target_groups, :languages).find(params[:id])
-
-    @placements = Placement.includes(refugee: [:gender, :countries, :municipality]).where(home_id: params[:id], moved_out_at: nil)
+    @placements = Placement.includes(refugee: [:gender, :countries, :municipality]).where(home_id: params[:id], moved_out_at: nil).order(:moved_in_at)
   end
 
   def new
