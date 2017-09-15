54a15b5d2db75bd8f7c7092560fa85b523b604fb
martent <marten@thavenius.se>
2017-09-15 09:56:09 +0200

    Fix

--- app/jobs/generate_report_job/economy.rb
+++ app/jobs/generate_report_job/economy.rb
@@ -1,11 +1,12 @@
 class GenerateReportJob
   class Economy < ApplicationJob
     def perform(params, file_id)
-      Reports::Economy.new(
+      workbook = Reports::Economy.new(
         filename: "#{file_id}.xlsx",
         from: params[:placements_from],
         to: params[:placements_to]
       )
+      workbook.create!
     end
   end
 end
--- app/jobs/generate_report_job/placements.rb
+++ app/jobs/generate_report_job/placements.rb
@@ -1,13 +1,14 @@
 class GenerateReportJob
   class Placements < ApplicationJob
     def perform(params, file_id)
-      Reports::Placements.new(
+      workbook = Reports::Placements.new(
         filename: "#{file_id}.xlsx",
         selection: params[:placements_selection],
         home_id: params[:placements_home_id],
         from: params[:placements_from],
         to: params[:placements_to]
-      ).create
+      )
+      workbook.create!
     end
   end
 end
