8c75f79aaa8beeb1feeff9062df7eac8a533cf7d
martent <marten@thavenius.se>
2017-10-01 18:01:13 +0200

    Fix form

--- spec/factories/rate_category.rb
+++ spec/factories/rate_category.rb
@@ -3,5 +3,6 @@ FactoryGirl.define do
     sequence(:name) { |n| "rate_category-#{n}" }
     sequence(:human_name) { |n| "rate_category-human-name#{n}" }
     sequence(:description) { |n| "desc-#{n}" }
+    sequence(:qualifier) { |n| n }
   end
 end
