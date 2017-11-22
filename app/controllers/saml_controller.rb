7f9a59a89a15da221c6a431a1e1ca92596f37fe4
martent <marten@thavenius.se>
2017-11-22 18:26:03 +0100

    Changed config strings to symbols

--- app/controllers/saml_controller.rb
+++ app/controllers/saml_controller.rb
@@ -73,16 +73,16 @@ class SamlController < ApplicationController
     # Metadata URI settings
     # Returns OneLogin::RubySaml::Settings prepopulated with idp metadata
     # idp_metadata_parser = OneLogin::RubySaml::IdpMetadataParser.new
-    # settings = idp_metadata_parser.parse_remote @config['idp_metadata']
+    # settings = idp_metadata_parser.parse_remote @config[:idp_metadata]
 
     settings = OneLogin::RubySaml::Settings.new
     settings.issuer                         = base_url
-    settings.idp_sso_target_url             = @config['idp_sso_target_url']
+    settings.idp_sso_target_url             = @config[:idp_sso_target_url]
     settings.assertion_consumer_service_url = "#{base_url}/saml/consume"
 
     # Non-metadata URI settings
-    # settings.idp_cert                       = @config['idp_cert']
-    settings.idp_cert_fingerprint           = @config['idp_cert_fingerprint']
+    # settings.idp_cert                       = @config[:idp_cert]
+    settings.idp_cert_fingerprint           = @config[:idp_cert_fingerprint]
     settings.idp_cert_fingerprint_algorithm = 'http://www.w3.org/2000/09/xmldsig#sha1'
     settings.name_identifier_format         = 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress'
     settings.authn_context                  = 'urn:oasis:names:tc:SAML:2.0:ac:classes:MobileTwoFactorUnregistered'
