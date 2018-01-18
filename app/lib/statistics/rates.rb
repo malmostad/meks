ec3b27ba45a984e4279dd9396e2aeadd7fe98fb7
martent <marten@thavenius.se>
2018-01-18 18:12:49 +0100

    Added blank checks for rates stats

--- app/lib/statistics/rates.rb
+++ app/lib/statistics/rates.rb
@@ -6,7 +6,7 @@ module Statistics
     # Return the number of days for each rate and the rate amount
     def self.for_all_rate_categories(refugee, range = DEFAULT_DATE_RANGE)
       @_rate_categories_and_rates ||= RateCategory.includes(:rates).all
-      @_rate_categories_and_rates.map do |category|
+      x = @_rate_categories_and_rates.map do |category|
         send(
           category.qualifier[:meth],
           refugee,
@@ -14,6 +14,10 @@ module Statistics
           range
         )
       end.flatten.compact
+      x.each do |y|
+        puts "#{y[:amount]} kr #{y[:days]} dagar"
+      end
+      x
     end
 
     def self.refugees_rates_for_category(category, range = DEFAULT_DATE_RANGE)
@@ -31,6 +35,12 @@ module Statistics
     end
 
     # Schablonkategori Ankomstbarn
+    #
+    # Måste:
+    #   inte ha datum medborgarskap
+    #     :citizenship_at
+    #   ha inskrivningsdatum idag eller tidigare
+    #     :registered
     #
     # Från-datum beräknas på senaste datum av följande:
     #    minimiålder
@@ -54,7 +64,8 @@ module Statistics
     # Return the number of days for each rate and the rate amount
     def self.arrival_0_17(refugee, category, range)
       return [] if
-          refugee.date_of_birth.nil?
+          refugee.date_of_birth.nil? ||
+          refugee.registered.nil?
 
       category.rates.map do |rate|
         from = latest_date(
@@ -120,6 +131,8 @@ module Statistics
     #     :temporary_permit_ends_at
     #   ha TUT som är längre än 12 månader
     #     :temporary_permit_starts_at, :temporary_permit_ends_at
+    #   ha Utskriven till Malmö
+    #     :checked_out_to_our_city
     #
     # Från-datum beräknas på senaste datum av följande:
     #   minimiålder
@@ -144,6 +157,7 @@ module Statistics
           refugee.date_of_birth.nil? ||
           refugee.temporary_permit_starts_at.nil? ||
           refugee.temporary_permit_ends_at.nil? ||
+          refugee.checked_out_to_our_city ||
           refugee.temporary_permit_ends_at - refugee.temporary_permit_starts_at < 365
 
       category.rates.map do |rate|
@@ -166,6 +180,14 @@ module Statistics
 
     # Schablonkategori PUT
     #
+    # Måste:
+    # Ha PUT
+    #   :residence_permit_at
+    # Ha Utskriven till Malmö
+    #   :checked_out_to_our_city
+    # inte ha
+    #   :citizenship_at
+    #
     # Från-datum beräknas på senaste datum av följande:
     #   minimiålder
     #     :date_of_birth
@@ -182,7 +204,11 @@ module Statistics
     #     :deregistered
     # Return the number of days for each rate and the rate amount
     def self.residence_permit(refugee, category, range)
-      return [] if refugee.date_of_birth.nil?
+      return [] if
+        refugee.date_of_birth.nil? ||
+        refugee.residence_permit_at.nil? ||
+        refugee.checked_out_to_our_city.nil? ||
+        refugee.citizenship_at.nil?
 
       category.rates.map do |rate|
         from = latest_date(
