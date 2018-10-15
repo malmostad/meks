d6301537dd2a38435e9c043b336f7ad0077c20a6
martent <marten@thavenius.se>
2018-10-15 15:23:19 +0200

    Calulation of extra contributions

--- app/lib/economy/extra_contribution.rb
+++ app/lib/economy/extra_contribution.rb
@@ -1,10 +1,46 @@
-# TODO: Specs
+# Extra insatser för barn
 module Economy
   class ExtraContribution < Base
-    def self.costs(refugee, range = DEFAULT_DATE_RANGE)
-      sum_per_month = refugee.extra_contributions.sum do |extra_contribution|
-        extra_contribution.fee + extra_contribution.expense
+    def initialize(refugee, report_range = DEFAULT_DATE_RANGE)
+      @refugee = refugee
+      @report_range = report_range
+    end
+
+    def as_formula
+      as_hash.map do |months_and_cost|
+        "(#{months_and_cost[:months]}*#{months_and_cost[:costs]})"
+      end.join('+')
+    end
+
+    def sum
+      as_hash.sum do |mac|
+        mac[:months] * mac[:costs]
+      end.round(2)
+    end
+
+    def as_hash
+      @refugee.extra_contributions.map do |extra_contribution|
+        range = date_range(extra_contribution)
+        {
+          months: months(range),
+          costs: (extra_contribution.fee + extra_contribution.expense).to_f
+        }
       end
+    end
+
+    private
+
+    def months(range)
+      (range[:from]..range[:to]).sum do |date|
+        1.to_f / (date.end_of_month - date.beginning_of_month + 1).to_f
+      end
+    end
+
+    def date_range(extra_contribution)
+      {
+        from: Base.latest_date(extra_contribution.period_start, @report_range[:from]),
+        to: Base.earliest_date(extra_contribution.period_end, @report_range[:to])
+      }
     end
   end
 end
