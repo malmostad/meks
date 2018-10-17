d3618a0a3810ff4d2be56e5a95c2c103ead4dc40
martent <marten@thavenius.se>
2018-10-17 12:06:57 +0200

    Added spec for temporary_permit_18_20

--- spec/unit/rate_temporary_permit_0_17_spec.rb
+++ spec/unit/rate_temporary_permit_0_17_spec.rb
@@ -1,6 +1,6 @@
 # Förväntad schablon för TUT 0-17
 # See specifications of conditions in app/lib/economy/rates.rb
-RSpec.describe 'Rates' do
+RSpec.describe 'Rates for temporary_permit_0_17' do
   before(:all) do
     create_rate_categories_with_rates
   end
@@ -11,120 +11,118 @@ RSpec.describe 'Rates' do
 
   let(:refugee) { create(:refugee) }
 
-  describe 'temporary_permit_0_17' do
-    before(:each) do
-      refugee.reload
-      # Mandatories
-      # refugee.temporary_permit_starts_at (defined below)
-      # refugee.temporary_permit_ends_at (defined below)
-      # refugee.checked_out_to_our_city (defined below)
+  before(:each) do
+    refugee.reload
+    # Mandatories
+    # refugee.temporary_permit_starts_at (defined below)
+    # refugee.temporary_permit_ends_at (defined below)
+    # refugee.checked_out_to_our_city (defined below)
 
-      # Count days from the last of the following
-      refugee.date_of_birth              = '2010-01-01'
-      refugee.checked_out_to_our_city    = '2018-04-01' # + 1 day
-      refugee.temporary_permit_starts_at = '2018-01-01'
+    # Count days from the last of the following
+    refugee.date_of_birth              = '2010-01-01'
+    refugee.checked_out_to_our_city    = '2018-04-01' # + 1 day
+    refugee.temporary_permit_starts_at = '2018-01-01'
 
-      # Count days to the first of the following
-      # refugee.date_of_birth + 1 year - 1 day (defined above)
-      refugee.temporary_permit_ends_at   = '2020-01-01'
-      refugee.residence_permit_at        = nil
-      refugee.deregistered               = nil # - 1 day
-    end
+    # Count days to the first of the following
+    # refugee.date_of_birth + 1 year - 1 day (defined above)
+    refugee.temporary_permit_ends_at   = '2020-01-01'
+    refugee.residence_permit_at        = nil
+    refugee.deregistered               = nil # - 1 day
+  end
 
-    it 'should have correct rate amount and days' do
-      rates = Economy::Rates.for_all_rate_categories(refugee, UnitMacros::REPORT_RANGE)
-      rate = detect_rate_by_amount(rates, UnitMacros::RATES[:temporary_permit_0_17])
+  it 'should have correct rate amount and days' do
+    rates = Economy::Rates.for_all_rate_categories(refugee, UnitMacros::REPORT_RANGE)
+    rate = detect_rate_by_amount(rates, UnitMacros::RATES[:temporary_permit_0_17])
 
-      expect(rate[:days]).to eq 91
-    end
+    expect(rate[:days]).to eq 91
+  end
 
-    it 'should respond to changed checked_out_to_our_city' do
-      refugee.checked_out_to_our_city = '2018-04-06'
+  it 'should respond to changed checked_out_to_our_city' do
+    refugee.checked_out_to_our_city = '2018-04-06'
 
-      rates = Economy::Rates.for_all_rate_categories(refugee, UnitMacros::REPORT_RANGE)
-      rate = detect_rate_by_amount(rates, UnitMacros::RATES[:temporary_permit_0_17])
+    rates = Economy::Rates.for_all_rate_categories(refugee, UnitMacros::REPORT_RANGE)
+    rate = detect_rate_by_amount(rates, UnitMacros::RATES[:temporary_permit_0_17])
 
-      expect(rate[:days]).to eq 87
-    end
+    expect(rate[:days]).to eq 87
+  end
 
-    it 'should respond to changed date_of_birth' do
-      refugee.date_of_birth = '2018-04-06'
+  it 'should respond to changed date_of_birth' do
+    refugee.date_of_birth = '2018-04-06'
 
-      rates = Economy::Rates.for_all_rate_categories(refugee, UnitMacros::REPORT_RANGE)
-      rate = detect_rate_by_amount(rates, UnitMacros::RATES[:temporary_permit_0_17])
+    rates = Economy::Rates.for_all_rate_categories(refugee, UnitMacros::REPORT_RANGE)
+    rate = detect_rate_by_amount(rates, UnitMacros::RATES[:temporary_permit_0_17])
 
-      expect(rate[:days]).to eq 86
-    end
+    expect(rate[:days]).to eq 86
+  end
 
-    it 'should respond to changed temporary_permit_starts_at' do
-      refugee.temporary_permit_starts_at = '2018-04-06'
+  it 'should respond to changed temporary_permit_starts_at' do
+    refugee.temporary_permit_starts_at = '2018-04-06'
 
-      rates = Economy::Rates.for_all_rate_categories(refugee, UnitMacros::REPORT_RANGE)
-      rate = detect_rate_by_amount(rates, UnitMacros::RATES[:temporary_permit_0_17])
+    rates = Economy::Rates.for_all_rate_categories(refugee, UnitMacros::REPORT_RANGE)
+    rate = detect_rate_by_amount(rates, UnitMacros::RATES[:temporary_permit_0_17])
 
-      expect(rate[:days]).to eq 86
-    end
+    expect(rate[:days]).to eq 86
+  end
 
-    it 'should respond to changed residence_permit_at' do
-      refugee.residence_permit_at = '2018-06-01'
+  it 'should respond to changed residence_permit_at' do
+    refugee.residence_permit_at = '2018-06-01'
 
-      rates = Economy::Rates.for_all_rate_categories(refugee, UnitMacros::REPORT_RANGE)
-      rate = detect_rate_by_amount(rates, UnitMacros::RATES[:temporary_permit_0_17])
+    rates = Economy::Rates.for_all_rate_categories(refugee, UnitMacros::REPORT_RANGE)
+    rate = detect_rate_by_amount(rates, UnitMacros::RATES[:temporary_permit_0_17])
 
-      expect(rate[:days]).to eq 62
-    end
+    expect(rate[:days]).to eq 62
+  end
 
-    it 'should respond to changed deregistered' do
-      refugee.deregistered = '2018-05-01'
+  it 'should respond to changed deregistered' do
+    refugee.deregistered = '2018-05-01'
 
-      rates = Economy::Rates.for_all_rate_categories(refugee, UnitMacros::REPORT_RANGE)
-      rate = detect_rate_by_amount(rates, UnitMacros::RATES[:temporary_permit_0_17])
+    rates = Economy::Rates.for_all_rate_categories(refugee, UnitMacros::REPORT_RANGE)
+    rate = detect_rate_by_amount(rates, UnitMacros::RATES[:temporary_permit_0_17])
 
-      expect(rate[:days]).to eq 30
-    end
+    expect(rate[:days]).to eq 30
+  end
 
-    it 'should respond to changed deregistered' do
-      refugee.temporary_permit_ends_at = '2018-05-01'
+  it 'should respond to changed deregistered' do
+    refugee.temporary_permit_ends_at = '2018-05-01'
 
-      rates = Economy::Rates.for_all_rate_categories(refugee, UnitMacros::REPORT_RANGE)
-      rate = detect_rate_by_amount(rates, UnitMacros::RATES[:temporary_permit_0_17])
+    rates = Economy::Rates.for_all_rate_categories(refugee, UnitMacros::REPORT_RANGE)
+    rate = detect_rate_by_amount(rates, UnitMacros::RATES[:temporary_permit_0_17])
 
-      expect(rate[:days]).to eq 31
-    end
+    expect(rate[:days]).to eq 31
+  end
 
-    it 'should respond to date_of_birth' do
-      refugee.date_of_birth = '2000-01-01'
+  it 'should respond to date_of_birth' do
+    refugee.date_of_birth = '2000-01-01'
 
-      rates = Economy::Rates.for_all_rate_categories(refugee, UnitMacros::REPORT_RANGE)
-      rate = detect_rate_by_amount(rates, UnitMacros::RATES[:temporary_permit_0_17])
+    rates = Economy::Rates.for_all_rate_categories(refugee, UnitMacros::REPORT_RANGE)
+    rate = detect_rate_by_amount(rates, UnitMacros::RATES[:temporary_permit_0_17])
 
-      expect(rate).to be_nil
-    end
+    expect(rate).to be_nil
+  end
 
-    it 'should require temporary_permit_starts_at' do
-      refugee.temporary_permit_starts_at = nil
+  it 'should require temporary_permit_starts_at' do
+    refugee.temporary_permit_starts_at = nil
 
-      rates = Economy::Rates.for_all_rate_categories(refugee, UnitMacros::REPORT_RANGE)
-      rate = detect_rate_by_amount(rates, UnitMacros::RATES[:temporary_permit_0_17])
+    rates = Economy::Rates.for_all_rate_categories(refugee, UnitMacros::REPORT_RANGE)
+    rate = detect_rate_by_amount(rates, UnitMacros::RATES[:temporary_permit_0_17])
 
-      expect(rate).to be_nil
-    end
+    expect(rate).to be_nil
+  end
 
-    it 'should require temporary_permit_ends_at' do
-      refugee.temporary_permit_ends_at = nil
+  it 'should require temporary_permit_ends_at' do
+    refugee.temporary_permit_ends_at = nil
 
-      rates = Economy::Rates.for_all_rate_categories(refugee, UnitMacros::REPORT_RANGE)
-      rate = detect_rate_by_amount(rates, UnitMacros::RATES[:temporary_permit_0_17])
+    rates = Economy::Rates.for_all_rate_categories(refugee, UnitMacros::REPORT_RANGE)
+    rate = detect_rate_by_amount(rates, UnitMacros::RATES[:temporary_permit_0_17])
 
-      expect(rate).to be_nil
-    end
+    expect(rate).to be_nil
+  end
 
-    it 'should require checked_out_to_our_city' do
-      refugee.checked_out_to_our_city = nil
-      rates = Economy::Rates.for_all_rate_categories(refugee, UnitMacros::REPORT_RANGE)
-      rate = detect_rate_by_amount(rates, UnitMacros::RATES[:temporary_permit_0_17])
+  it 'should require checked_out_to_our_city' do
+    refugee.checked_out_to_our_city = nil
+    rates = Economy::Rates.for_all_rate_categories(refugee, UnitMacros::REPORT_RANGE)
+    rate = detect_rate_by_amount(rates, UnitMacros::RATES[:temporary_permit_0_17])
 
-      expect(rate).to be_nil
-    end
+    expect(rate).to be_nil
   end
 end
---
+++ spec/unit/rate_temporary_permit_18_20_spec.rb
@@ -0,0 +1,57 @@
+# Förväntad schablon för TUT 18-20
+#
+# See specifications of conditions in app/lib/economy/rates.rb
+#
+# The majority of test for this rate category is performed
+# in rate_temporary_permit_0_17_spec.rb. Only conditionals for date of birth differs
+RSpec.describe 'Rates for temporary_permit_18_20' do
+  before(:all) do
+    create_rate_categories_with_rates
+  end
+
+  let(:refugee) { create(:refugee) }
+
+  before(:each) do
+    refugee.reload
+    # Mandatories
+    # refugee.temporary_permit_starts_at (defined below)
+    # refugee.temporary_permit_ends_at (defined below)
+    # refugee.checked_out_to_our_city (defined below)
+
+    # Count days from the last of the following
+    refugee.date_of_birth              = '2000-01-01'
+    refugee.checked_out_to_our_city    = '2018-04-01' # + 1 day
+    refugee.temporary_permit_starts_at = '2018-01-01'
+
+    # Count days to the first of the following
+    # refugee.date_of_birth + 1 year - 1 day (defined above)
+    refugee.temporary_permit_ends_at   = '2020-01-01'
+    refugee.residence_permit_at        = nil
+    refugee.deregistered               = nil # - 1 day
+  end
+
+  it 'should have correct rate amount and days' do
+    rates = Economy::Rates.for_all_rate_categories(refugee, UnitMacros::REPORT_RANGE)
+    rate = detect_rate_by_amount(rates, UnitMacros::RATES[:temporary_permit_18_20])
+
+    expect(rate[:days]).to eq 91
+  end
+
+  it 'should respond to changed date_of_birth' do
+    refugee.date_of_birth = '2000-04-06'
+
+    rates = Economy::Rates.for_all_rate_categories(refugee, UnitMacros::REPORT_RANGE)
+    rate = detect_rate_by_amount(rates, UnitMacros::RATES[:temporary_permit_18_20])
+
+    expect(rate[:days]).to eq 86
+  end
+
+  it 'should respond to date_of_birth' do
+    refugee.date_of_birth = '2001-01-01'
+
+    rates = Economy::Rates.for_all_rate_categories(refugee, UnitMacros::REPORT_RANGE)
+    rate = detect_rate_by_amount(rates, UnitMacros::RATES[:temporary_permit_18_20])
+
+    expect(rate).to be_nil
+  end
+end
