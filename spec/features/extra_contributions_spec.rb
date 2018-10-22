798d8d8f51e77389336fb69544539c786a599379
martent <marten@thavenius.se>
2018-10-22 15:51:12 +0200

    More changes from 'extra kostnaden' to 'extra insatsen/extra kostnaden'

--- spec/features/extra_contributions_spec.rb
+++ spec/features/extra_contributions_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe 'ExtraContributions', type: :feature do
         extra_contribution_types = create_list(:extra_contribution_type, 3)
 
         visit "/refugees/#{refugee.id}"
-        click_on 'Ny extra insats'
+        click_on 'Ny extra insats/extra kostnad'
         expect(current_path).to eq new_refugee_extra_contribution_path(refugee)
 
         select(extra_contribution_types[1].name, from: 'extra_contribution_extra_contribution_type_id')
@@ -21,7 +21,7 @@ RSpec.describe 'ExtraContributions', type: :feature do
         click_button 'Spara'
 
         expect(current_path).to eq refugee_path(refugee)
-        expect(page).to have_selector('.notice', text: 'Extra insatsen registrerades')
+        expect(page).to have_selector('.notice', text: 'Extra insatsen/extra kostnaden registrerades')
         expect(page).to have_selector('.extra_contribution .controls', text: extra_contribution_types[1].name)
       end
     end
@@ -33,7 +33,7 @@ RSpec.describe 'ExtraContributions', type: :feature do
         create(:extra_contribution, refugee: refugee, extra_contribution_type: extra_contribution_types.first)
 
         visit "/refugees/#{refugee.id}"
-        click_link('Redigera extra insatsen')
+        click_link('Redigera extra insatsen/extra kostnaden')
         expect(current_path).to eq edit_refugee_extra_contribution_path(refugee, refugee.extra_contributions.first)
 
         select(extra_contribution_types[2].name, from: 'extra_contribution_extra_contribution_type_id')
@@ -44,7 +44,7 @@ RSpec.describe 'ExtraContributions', type: :feature do
         click_button 'Spara'
 
         expect(current_path).to eq refugee_path(refugee)
-        expect(page).to have_selector('.notice', text: 'Extra insatsen uppdaterades')
+        expect(page).to have_selector('.notice', text: 'Extra insatsen/extra kostnaden uppdaterades')
         expect(page).to have_selector('.extra_contribution .controls', text: extra_contribution_types[2].name)
       end
     end
