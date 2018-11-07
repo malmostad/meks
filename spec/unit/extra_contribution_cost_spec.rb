de575cd01318e0cf4e20624b28699d9f0668fcb1
martent <marten@thavenius.se>
2018-11-07 11:12:22 +0100

    Specs for refugees extra costs

--- spec/unit/extra_contribution_cost_spec.rb
+++ spec/unit/extra_contribution_cost_spec.rb
@@ -1,4 +1,4 @@
-RSpec.describe 'ExtraContribution' do
+RSpec.describe 'ExtraContributionCost' do
   let(:refugee) { create(:refugee) }
   let(:extra_contribution) do
     create(
@@ -17,44 +17,44 @@ RSpec.describe 'ExtraContribution' do
   end
 
   it 'should have correct cost for a complete extra_contribution period' do
-    ec = Economy::ExtraContribution.new(
+    ecc = Economy::ExtraContributionCost.new(
       refugee,
       from: '2018-01-01',
       to: '2018-12-31'
     )
-    expect(ec.sum).to eq 429_612
+    expect(ecc.sum).to eq 429_612
   end
 
   it 'should use default report range' do
-    ec = Economy::ExtraContribution.new(refugee)
-    expect(ec.sum).to be_a(Numeric)
+    ecc = Economy::ExtraContributionCost.new(refugee)
+    expect(ecc.sum).to be_a(Numeric)
   end
 
   it 'should have correct cost for a limiting full month report period' do
-    ec = Economy::ExtraContribution.new(
+    ecc = Economy::ExtraContributionCost.new(
       refugee,
       from: '2018-01-01',
       to: '2018-03-31'
     )
-    expect(ec.sum).to eq 107_403
+    expect(ecc.sum).to eq 107_403
   end
 
   it 'should have correct cost for a limiting partial month report period' do
-    ec = Economy::ExtraContribution.new(
+    ecc = Economy::ExtraContributionCost.new(
       refugee,
       from: '2018-01-01',
       to: '2018-01-10'
     )
-    expect(ec.sum).to eq 11_548.71
+    expect(ecc.sum).to eq 11_548.71
   end
 
   it 'should have correct cost formula' do
-    ec = Economy::ExtraContribution.new(
+    ecc = Economy::ExtraContributionCost.new(
       refugee,
       from: '2018-01-01',
       to: '2018-01-10'
     )
-    expect(ec.as_formula_array).to eq ['0.3225806451612903*35801.0']
+    expect(ecc.as_formula_array).to eq ['0.3225806451612903*35801.0']
   end
 
   describe 'multiple extra contributions' do
@@ -70,21 +70,21 @@ RSpec.describe 'ExtraContribution' do
     end
 
     it 'should have correct cost for a limiting partial month over several years report period' do
-      ec = Economy::ExtraContribution.new(
+      ecc = Economy::ExtraContributionCost.new(
         refugee,
         from: '2017-06-11',
         to: '2018-04-15'
       )
-      expect(ec.sum).to eq 1_217_478.67
+      expect(ecc.sum).to eq 1_217_478.67
     end
 
     it 'should have correct cost formula' do
-      ec = Economy::ExtraContribution.new(
+      ecc = Economy::ExtraContributionCost.new(
         refugee,
         from: '2017-06-11',
         to: '2018-04-15'
       )
-      expect(ec.as_formula_array).to eq ['10.166666666666666*119752.0']
+      expect(ecc.as_formula_array).to eq ['10.166666666666666*119752.0']
     end
   end
 end
---
+++ spec/unit/refugee_extra_cost_spec.rb
@@ -0,0 +1,95 @@
+RSpec.describe 'RefugeeExtraCost' do
+  describe 'single cost' do
+    let(:refugee) { create(:refugee) }
+
+    let(:refugee_extra_cost) do
+      create(
+        :refugee_extra_cost,
+        date: '2018-03-01',
+        amount: 1234,
+        refugee: refugee
+      )
+    end
+
+    before(:each) do
+      refugee.reload
+      refugee_extra_cost.reload
+    end
+
+    it 'should have correct cost for a refugee_extra_cost period' do
+      rec = Economy::RefugeeExtraCost.new(
+        refugee,
+        from: '2018-01-01',
+        to: '2018-12-31'
+      )
+      expect(rec.sum).to eq 1234
+    end
+
+    it 'should use default report range' do
+      rec = Economy::RefugeeExtraCost.new(refugee)
+      expect(rec.sum).to be_a(Numeric)
+    end
+
+    it 'should have correct cost for a limiting full month report period' do
+      rec = Economy::RefugeeExtraCost.new(
+        refugee,
+        from: '2018-01-01',
+        to: '2018-03-31'
+      )
+      expect(rec.sum).to eq 1234
+    end
+
+    it 'should have a zero cost for a period without cost' do
+      rec = Economy::RefugeeExtraCost.new(
+        refugee,
+        from: '2018-01-01',
+        to: '2018-01-10'
+      )
+      expect(rec.sum).to eq 0
+    end
+  end
+
+  describe 'multiple costs' do
+    let(:refugee) { create(:refugee) }
+
+    let(:refugee_extra_costs) do
+      [
+        create(
+          :refugee_extra_cost,
+          date: '2018-03-01',
+          amount: 1234,
+          refugee: refugee
+        ),
+        create(
+          :refugee_extra_cost,
+          date: '2018-03-10',
+          amount: 2345,
+          refugee: refugee
+        )
+      ]
+    end
+
+    before(:each) do
+      refugee.reload
+      refugee_extra_costs.each(&:reload)
+    end
+
+    it 'should have correct cost sum' do
+      rec = Economy::RefugeeExtraCost.new(
+        refugee,
+        from: '2018-01-01',
+        to: '2018-12-31'
+      )
+      expect(rec.sum).to eq 3579
+    end
+
+    it 'should have correct cost formula' do
+      rec = Economy::RefugeeExtraCost.new(
+        refugee,
+        from: '2018-01-01',
+        to: '2018-12-31'
+      )
+      expect(rec.as_formula_array).to eq ['1234.0', '2345.0']
+    end
+  end
+end
