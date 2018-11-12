0afbf356d8a2d005911d712e898cb28df350a309
martent <marten@thavenius.se>
2018-11-12 13:56:17 +0100

    Feature specs for payment imports

--- spec/features/payment_imports_spec.rb
+++ spec/features/payment_imports_spec.rb
@@ -1,65 +1,78 @@
 RSpec.feature PaymentImport, type: :feature do
-  # TODO: below is pasted code from Language feat spec
+  feature 'admin role' do
+    before(:each) do
+      login_user(:admin)
+    end
 
-  # feature "admin role" do
-  #   before(:each) do
-  #     login_user(:admin)
-  #   end
-  #
-  #   scenario "adds a language" do
-  #     visit "/languages/new"
-  #     fill_in "language_name", with: "Foo"
-  #     click_button "Spara"
-  #
-  #     expect(current_path).to eq languages_path
-  #     expect(page).to have_selector(".notice", text: "skapades")
-  #   end
-  #
-  #   scenario "updates a language" do
-  #     language = create(:language, name: "Foo")
-  #     visit edit_language_path(language)
-  #     fill_in "language_name", with: "Bar"
-  #     click_button "Spara"
-  #
-  #     expect(current_path).to eq languages_path
-  #     expect(page).to have_selector(".notice", text: "uppdaterades")
-  #     expect(page).not_to have_selector("td", text: "Foo")
-  #     expect(page).to have_selector("td", text: "Bar")
-  #   end
-  #
-  #   scenario "deletes a language", js: true do
-  #     language = create(:language, name: "Fox")
-  #     visit "/languages"
-  #     page.accept_alert 'Är du säker?' do
-  #       first(".btn-danger").click
-  #     end
-  #     expect(page).to have_selector(".notice", text: "raderades")
-  #   end
-  # end
-  #
-  # feature "reader role" do
-  #   before(:each) do
-  #     login_user(:reader)
-  #   end
-  #
-  #   scenario "can't add a language" do
-  #     visit "/languages/new"
-  #
-  #     expect(current_path).to eq root_path
-  #     expect(page).to have_selector(".alert", text: "Din roll saknar behörighet")
-  #   end
-  # end
-  #
-  # feature "writer role" do
-  #   before(:each) do
-  #     login_user(:writer)
-  #   end
-  #
-  #   scenario "can't add a language" do
-  #     visit "/languages/new"
-  #
-  #     expect(current_path).to eq root_path
-  #     expect(page).to have_selector(".alert", text: "Din roll saknar behörighet")
-  #   end
-  # end
+    scenario 'uploads a valid payment file' do
+      create(:refugee,
+             dossier_number: '8254980244',
+             municipality: create(:municipality, our_municipality: true)
+      )
+
+      visit '/payment_imports'
+      click_link 'Lägg till'
+      expect(current_path).to eq new_payment_import_path
+
+      attach_file('payment_import[file]', File.join(fixture_path, 'payments.csv'))
+
+      click_button 'Spara'
+      expect(current_path).to eq payment_imports_path
+      expect(page).to have_selector('.notice', text: 'Importen sparades')
+    end
+
+    scenario 'uploads a valid payment file' do
+      create(:refugee, dossier_number: '1234')
+
+      visit '/payment_imports'
+      click_link 'Lägg till'
+      expect(current_path).to eq new_payment_import_path
+
+      attach_file('payment_import[file]', File.join(fixture_path, 'payments.csv'))
+
+      click_button 'Spara'
+      expect(current_path).to eq payment_imports_path
+      expect(page).to have_selector('.warning', text: 'Inget barn hittades med dossiernumret')
+    end
+
+    scenario 'deletes an import', js: true do
+      payment_import = create(:payment_import)
+      visit '/payment_imports'
+
+      click_link 'Visa'
+      expect(current_path).to eq payment_import_path(payment_import.id)
+
+      accept_alert do
+        click_button 'Radera permanent'
+      end
+
+      expect(page).to have_selector('.notice', text: 'raderades')
+    end
+  end
+
+  feature 'reader role' do
+    before(:each) do
+      login_user(:reader)
+    end
+
+    scenario 'can’t add a language' do
+      visit '/payment_imports'
+
+      expect(current_path).to eq root_path
+      expect(page).to have_selector('.alert', text: 'Din roll saknar behörighet')
+    end
+  end
+
+  feature 'writer role' do
+    before(:each) do
+      login_user(:writer)
+    end
+
+    scenario 'can’t add a language' do
+      visit '/payment_imports'
+
+      expect(current_path).to eq root_path
+      expect(page).to have_selector('.alert', text: 'Din roll saknar behörighet')
+    end
+  end
 end
