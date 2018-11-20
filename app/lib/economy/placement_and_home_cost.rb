bcb846e9f48bf1b99726c51fe2292a4ae6ed8f33
martent <marten@thavenius.se>
2018-11-20 15:24:04 +0100

    Removed first alt of cost_for_family_and_emergency_home implementation

--- app/lib/economy/placement_and_home_cost.rb
+++ app/lib/economy/placement_and_home_cost.rb
@@ -2,7 +2,10 @@ module Economy
   # Calculates the following cost in the given interval for a number of placements:
   #   1. The homes daily cost if the placements home is of cost type #cost_per_day?
   #   2. The placements cost if the placements home is of cost type #cost_per_placement?
-  #   3. The placements family_and_emergency_costs if the placements home is of cost type #cost_for_family_and_emergency_home?
+  #
+  #  Note that placements with home that has cost type #cost_for_family_and_emergency_home?
+  #    are calculated in Economy::FamilyAndEmergencyHomeCost since the structure different
+  #    from #cost_per_day? and #cost_per_placement?
   class PlacementAndHomeCost < Base
     def initialize(placements, interval = DEFAULT_INTERVAL)
       @placements = placements
@@ -22,9 +25,8 @@ module Economy
         args = [placement, from: from, to: to]
 
         # Which type_of_cost?
-        next placement_home_costs(*args)       if placement.home.cost_per_day?
-        next placement_cost(*args)             if placement.home.cost_per_placement?
-        # next family_and_emergency_costs(*args) if placement.home.cost_for_family_and_emergency_home?
+        next placement_home_costs(*args) if placement.home.cost_per_day?
+        next placement_cost(*args) if placement.home.cost_per_placement?
       end.flatten.compact
     end
 
@@ -41,19 +43,6 @@ module Economy
         to   = earliest_date(interval[:to], cost.end_date)
         days = number_of_days(from, to)
         { amount: cost.amount.to_i, days: days }
-      end
-    end
-
-    # Costs for family and emergency home
-    def family_and_emergency_costs(placement, interval)
-      placement.family_and_emergency_home_costs.map do |cost|
-        from = latest_date(interval[:from], cost.period_start)
-        to   = earliest_date(interval[:to], cost.period_end)
-        months = number_of_months(from: from, to: to)
-        {
-          months: months,
-          amount: (cost.fee + cost.expense).to_f
-        }
       end
     end
   end
