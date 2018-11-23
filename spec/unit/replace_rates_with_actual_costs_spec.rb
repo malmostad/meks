41687e1bcf278d4bfa31f4c3621a68d461510b2a
martent <marten@thavenius.se>
2018-11-23 15:15:41 +0100

    Added specs for ReplaceRatesWithActualCosts

---
+++ spec/unit/replace_rates_with_actual_costs_spec.rb
@@ -0,0 +1,38 @@
+RSpec.describe Economy::ReplaceRatesWithActualCosts do
+  let(:refugee) { create(:refugee) }
+  let(:home) { create(:home, type_of_cost: :cost_per_placement) }
+  let(:placement) do
+    create(
+      :placement_with_rate_exempt,
+      refugee: refugee,
+      home: home,
+      moved_in_at: '2018-03-15',
+      moved_out_at: '2018-06-20',
+      cost: 1234
+    )
+  end
+
+  before(:each) do
+    refugee.reload
+    home.reload
+    placement.reload
+  end
+
+  it 'should calulate number of days and rate' do
+    expect(Economy::ReplaceRatesWithActualCosts.new(refugee).sum).to eq((97 + 1) * 1234)
+  end
+
+  it 'should calulate number of days and rate limited by report interval' do
+    expect(Economy::ReplaceRatesWithActualCosts.new(
+      refugee, from: '2018-02-01', to: '2018-03-31'
+    ).sum).to eq((16 + 1) * 1234)
+  end
+
+  it 'should calulate number of days and rate and return a string' do
+    expect(Economy::ReplaceRatesWithActualCosts.new(refugee).as_formula).to eq '98*1234'
+  end
+
+  it 'should have a reduced number of days rate' do
+    expect(Economy::ReplaceRatesWithActualCosts.new(refugee).as_array).to eq([days: 98, amount: 1234])
+  end
+end
