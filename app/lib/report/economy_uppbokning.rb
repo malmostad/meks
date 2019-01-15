7d56d9c40ed5045ba0c4a4f8cb947b098b2b8199
martent <marten@thavenius.se>
2019-01-15 18:17:11 +0100

    Preparing for sub sheet data

--- app/lib/report/economy_uppbokning.rb
+++ app/lib/report/economy_uppbokning.rb
@@ -2,32 +2,76 @@ module Report
   class EconomyUppbokning < Workbooks
     def initialize(options = {})
       super(options)
+
+      # Rate categories (Schablonkategorier)
+      @assigned_0_17 = RateCategory.where(name: 'assigned_0_17').first
+      @tut_0_17      = RateCategory.where(name: 'temporary_permit_0_17').first
+      @put_0_17      = RateCategory.where(name: 'residence_permit_0_17').first
+      @tut_18_20     = RateCategory.where(name: 'temporary_permit_18_20').first
+      @put_18_20     = RateCategory.where(name: 'residence_permit_18_20').first
+      @arrival_0_17  = RateCategory.where(name: 'arrival_0_17').first
+
+      # Refugees with placements with specific legal codes by ID(s) (Lagrum)
+      @sol = refugees_with_legal_code(1)
+      @lvu_and_sol_lvu = refugees_with_legal_code(2, 3)
+      @all = refugees_with_legal_code # All refugees
     end
 
     def records
-      # Rate categories (Schablonkategorier)
-      assigned_0_17 = RateCategory.where(name: 'assigned_0_17').first
-      tut_0_17      = RateCategory.where(name: 'temporary_permit_0_17').first
-      put_0_17      = RateCategory.where(name: 'residence_permit_0_17').first
-      tut_18_20     = RateCategory.where(name: 'temporary_permit_18_20').first
-      put_18_20     = RateCategory.where(name: 'residence_permit_18_20').first
-      arrival_0_17  = RateCategory.where(name: 'arrival_0_17').first
+      sub_sheets.map do |sub_sheet|
+        {
+          name: sub_sheet[:name],
+          refugees: sub_sheet[:refugees]
+        }
+      end
+    end
 
-      # Refugees with placements with specific legal codes by ID(s) (Lagrum)
-      sol = refugees_with_legal_code(1)
-      lvu_and_sol_lvu = refugees_with_legal_code(2, 3)
-      all = refugees_with_legal_code # All refugees
+    def columns(record = { name: nil, refugees: [] }, i = 0)
+      [
+        {
+          heading: 'Barnens status',
+          query: record[:name]
+        },
+        {
+          heading: 'Förväntad intäkt',
+          query: self.class.sum_formula(record[:refugees].sum { |r| r[:data].sum { |d| d[:amount] * d[:days] } }),
+          style: 'currency'
+        }
+      ]
+    end
 
-      # Refugees with combination of given rate categories and legal codes (as above)
-      {
-        sol_assigned_0_17: per_rate_category(sol, assigned_0_17),
-        sol_tut_put_0_17: per_rate_category(sol, tut_0_17, put_0_17),
-        sol_tut_put_18_20: per_rate_category(sol, tut_18_20, put_18_20),
-        lvu_and_sol_lvu_assigned_0_17: per_rate_category(lvu_and_sol_lvu, assigned_0_17),
-        lvu_and_sol_lvu_tut_put_0_17: per_rate_category(lvu_and_sol_lvu, tut_0_17, put_0_17),
-        lvu_and_sol_lvu_tut_put_18_20: per_rate_category(lvu_and_sol_lvu, tut_18_20, put_18_20),
-        arrival: per_rate_category(all, arrival_0_17)
-      }
+    # Refugees with combination of given rate categories and legal codes (as above)
+    def sub_sheets
+      [
+        {
+          name: 'SoL, Anvisade barn, 0–17',
+          refugees: per_rate_category(@sol, @assigned_0_17)
+        },
+        {
+          name: 'SoL, PUT+TUT, 0–17',
+          refugees: per_rate_category(@sol, @tut_0_17, @put_0_17)
+        },
+        {
+          name: 'SoL, PUT+TUT, 18–20',
+          refugees: per_rate_category(@sol, @tut_18_20, @put_18_20)
+        },
+        {
+          name: 'LVU+SoL med LVU, Anvisade 0–17',
+          refugees: per_rate_category(@lvu_and_sol_lvu, @assigned_0_17)
+        },
+        {
+          name: 'LVU+SoL med LVU, PUT 0–17',
+          refugees: per_rate_category(@lvu_and_sol_lvu, @tut_0_17, @put_0_17)
+        },
+        {
+          name: 'LVU+SoL med LVU, PUT 18–20',
+          refugees: per_rate_category(@lvu_and_sol_lvu, @tut_18_20, @put_18_20)
+        },
+        {
+          name: 'Alla lagrum, Ankomstbarn, 0–17',
+          refugees: per_rate_category(@all, @arrival_0_17)
+        }
+      ]
     end
 
     def per_rate_category(refugees, *categories)
@@ -64,7 +108,7 @@ module Report
       refugees
     end
 
-    def columns(refugee = Refugee.new, i = 0)
+    def sub_sheet_columns(refugee = Refugee.new, i = 0)
       row = i + 2
 
       [
@@ -97,16 +141,6 @@ module Report
           heading: 'Antal dygn',
           query: 'todo'
         }
-      ]
-    end
-
-    def last_row(row)
-      [
-        'SUMMA:',
-        '',
-        '',
-        '',
-        "=SUM(E2:E#{row})"
       ]
     end
   end
