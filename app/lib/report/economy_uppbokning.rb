c30c37c0fcf81274cfeed944bbb1532d6df3d233
martent <marten@thavenius.se>
2019-01-17 11:20:39 +0100

    Refactoring

--- app/lib/report/economy_uppbokning.rb
+++ app/lib/report/economy_uppbokning.rb
@@ -9,20 +9,6 @@ module Report
   class EconomyUppbokning < Workbooks
     def initialize(options = {})
       super(options)
-
-      # Rate categories (Schablonkategorier)
-      @assigned_0_17 = RateCategory.where(name: 'assigned_0_17').first
-      @tut_0_17      = RateCategory.where(name: 'temporary_permit_0_17').first
-      @put_0_17      = RateCategory.where(name: 'residence_permit_0_17').first
-      @tut_18_20     = RateCategory.where(name: 'temporary_permit_18_20').first
-      @put_18_20     = RateCategory.where(name: 'residence_permit_18_20').first
-      @arrival_0_17  = RateCategory.where(name: 'arrival_0_17').first
-
-      # Refugees with placements with specific legal codes by ID(s) (Lagrum)
-      @sol             = refugees_with_legal_code(1)
-      @lvu_and_sol_lvu = refugees_with_legal_code(2, 3)
-      @all             = refugees_with_legal_code # All refugees
-
       @statuses = statuses
     end
 
@@ -78,34 +64,47 @@ module Report
 
     # Refugees with combination of given rate categories and legal codes (as above)
     def statuses
+      # Rate categories (Schablonkategorier)
+      assigned_0_17   = RateCategory.where(name: 'assigned_0_17').first
+      tut_0_17        = RateCategory.where(name: 'temporary_permit_0_17').first
+      put_0_17        = RateCategory.where(name: 'residence_permit_0_17').first
+      tut_18_20       = RateCategory.where(name: 'temporary_permit_18_20').first
+      put_18_20       = RateCategory.where(name: 'residence_permit_18_20').first
+      arrival_0_17    = RateCategory.where(name: 'arrival_0_17').first
+
+      # Refugees with placements with specific legal codes by ID(s) (Lagrum)
+      sol             = refugees_with_legal_code(1)
+      lvu_and_sol_lvu = refugees_with_legal_code(2, 3)
+      all             = refugees_with_legal_code # All refugees
+
       [
         {
           name: 'SoL, Anvisade barn, 0–17',
-          refugees: per_rate_category(@sol, @assigned_0_17)
+          refugees: per_rate_category(sol, assigned_0_17)
         },
         {
           name: 'SoL, PUT+TUT, 0–17',
-          refugees: per_rate_category(@sol, @tut_0_17, @put_0_17)
+          refugees: per_rate_category(sol, tut_0_17, put_0_17)
         },
         {
           name: 'SoL, PUT+TUT, 18–20',
-          refugees: per_rate_category(@sol, @tut_18_20, @put_18_20)
+          refugees: per_rate_category(sol, tut_18_20, put_18_20)
         },
         {
           name: 'LVU+SoL med LVU, Anvisade 0–17',
-          refugees: per_rate_category(@lvu_and_sol_lvu, @assigned_0_17)
+          refugees: per_rate_category(lvu_and_sol_lvu, assigned_0_17)
         },
         {
           name: 'LVU+SoL med LVU, PUT 0–17',
-          refugees: per_rate_category(@lvu_and_sol_lvu, @tut_0_17, @put_0_17)
+          refugees: per_rate_category(lvu_and_sol_lvu, tut_0_17, put_0_17)
         },
         {
           name: 'LVU+SoL med LVU, PUT 18–20',
-          refugees: per_rate_category(@lvu_and_sol_lvu, @tut_18_20, @put_18_20)
+          refugees: per_rate_category(lvu_and_sol_lvu, tut_18_20, put_18_20)
         },
         {
           name: 'Alla lagrum, Ankomstbarn, 0–17',
-          refugees: per_rate_category(@all, @arrival_0_17)
+          refugees: per_rate_category(all, arrival_0_17)
         }
       ]
     end
