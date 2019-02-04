0dd7ff71850c0b09a8eb8aef67bb772a3e07e52a
martent <marten@thavenius.se>
2019-02-04 15:07:30 +0100

    Updated factory spec

--- spec/factories/po_rate.rb
+++ spec/factories/po_rate.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :po_rate do
     rate_under_65 30.32
     rate_between_65_and_81 30.64
+    rate_from_82 30.64
     start_date '2019-01-01'
     end_date '2019-12-31'
   end
