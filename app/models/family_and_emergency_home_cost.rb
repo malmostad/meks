47490eed28dc59453d83d494ad0917cf23d72f2d
martent <marten@thavenius.se>
2019-02-12 15:18:59 +0100

    Allow blank amount in family costs

--- app/models/family_and_emergency_home_cost.rb
+++ app/models/family_and_emergency_home_cost.rb
@@ -2,7 +2,7 @@
 class FamilyAndEmergencyHomeCost < ApplicationRecord
   belongs_to :placement, touch: true
 
-  validates_presence_of :period_start, :period_end, :fee, :expense,
+  validates_presence_of :period_start, :period_end, :fee,
                         :contractor_name, :contractor_birthday, :contactor_employee_number
-  validates :fee, :expense, :contactor_employee_number, numericality: true
+  validates :fee, :contactor_employee_number, numericality: true
 end
