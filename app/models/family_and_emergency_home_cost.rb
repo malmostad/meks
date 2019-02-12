18602d6456454a3c629278e3dafb4150e90cf36d
martent <marten@thavenius.se>
2019-02-12 15:58:58 +0100

    Fixed conditional validation of numericality

--- app/models/family_and_emergency_home_cost.rb
+++ app/models/family_and_emergency_home_cost.rb
@@ -5,4 +5,5 @@ class FamilyAndEmergencyHomeCost < ApplicationRecord
   validates_presence_of :period_start, :period_end, :fee,
                         :contractor_name, :contractor_birthday, :contactor_employee_number
   validates :fee, :contactor_employee_number, numericality: true
+  validates :expense, numericality: true, allow_blank: true
 end
