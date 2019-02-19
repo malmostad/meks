1467f50276bf047bd95f137b0e449bfb0c7ee6c3
martent <marten@thavenius.se>
2019-02-19 11:41:22 +0100

    Added Procapita customer number to eco reports

--- app/reports/helpers/report_helper.rb
+++ app/reports/helpers/report_helper.rb
@@ -1,6 +1,6 @@
 module ReportHelper
-  def heading_comment(sheet, col, comment)
-    sheet.add_comment(ref: "#{col}1", text: comment, author: '', visible: false)
+  def heading_comment(sheet, col, comment, row = 1)
+    sheet.add_comment(ref: "#{col}#{row}", text: comment, author: '', visible: false)
   end
 
   def sum_formula(*arr)
