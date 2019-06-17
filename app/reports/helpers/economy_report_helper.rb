801c832f37655aeb0f6d33b467ea08d1317c1992
martent <marten@thavenius.se>
2019-06-17 12:07:56 +0200

    Moved some report helpers from economy to report heler

--- app/reports/helpers/economy_report_helper.rb
+++ app/reports/helpers/economy_report_helper.rb
@@ -6,26 +6,4 @@ module EconomyReportHelper
   def payment_comments(refugee, interval)
     ::Economy::Payment.new(refugee.payments, interval).comments.join("\r\x0D\x0A")
   end
-
-  def placement_names_within_range(refugee, interval)
-    placements_within_range(refugee, interval).map do |pl|
-      "#{pl.home.name} (#{pl.moved_in_at}–#{pl.moved_out_at})"
-    end.join(', ')
-  end
-
-  def legal_codes_within_range(refugee, interval)
-    placements_within_range(refugee, interval)
-      .map(&:legal_code)
-      .try(:map, &:name).join(', ')
-  end
-
-  # Filter out placements within the report range without add additional queries
-  def placements_within_range(refugee, interval)
-    refugee.placements.map do |placement|
-      if placement.moved_in_at <= interval[:to].to_date &&
-         (placement.moved_out_at.nil? || placement.moved_out_at >= interval[:from].to_date)
-        next placement
-      end
-    end.compact.sort_by(&:moved_in_at)
-  end
 end
--- app/reports/helpers/report_helper.rb
+++ app/reports/helpers/report_helper.rb
@@ -34,6 +34,28 @@ module ReportHelper
     )
   end
 
+  def placement_names_within_range(refugee, interval)
+    placements_within_range(refugee, interval).map do |pl|
+      "#{pl.home.name} (#{pl.moved_in_at}–#{pl.moved_out_at})"
+    end.join(', ')
+  end
+
+  def legal_codes_within_range(refugee, interval)
+    placements_within_range(refugee, interval)
+      .map(&:legal_code)
+      .try(:map, &:name).join(', ')
+  end
+
+  # Filter out placements within the report range without add additional queries
+  def placements_within_range(refugee, interval)
+    refugee.placements.map do |placement|
+      if placement.moved_in_at <= interval[:to].to_date &&
+         (placement.moved_out_at.nil? || placement.moved_out_at >= interval[:from].to_date)
+        next placement
+      end
+    end.compact.sort_by(&:moved_in_at)
+  end
+
   def step_sum(row, range, step)
     range.step(step).map { |x| "#{Axlsx.col_ref(x)}#{row}" }.join('+')
   end
