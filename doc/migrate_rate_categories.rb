598c881572ac3676faeaef9d72f8057ce16b5c61
martent <marten@thavenius.se>
2021-04-07 17:23:57 +0200

    Doc

--- doc/migrate_rate_categories.rb
+++ doc/migrate_rate_categories.rb
@@ -1,6 +1,35 @@
-rate_categories = %w[arrival_0_17 assigned_0_17 temporary_permit_0_17 temporary_permit_18_20  residence_permit_0_17 residence_permit_18_20]
+RateCategory.where(name: "arrival_0_17").first.update(
+  qualifier: "arrival_0_17",
+  min_age: 0,
+  max_age: 17
+)
 
-rate_categories.each do |rc|
-  r = RateCategory.where(name: rc).first
-  r.update_attributes(qualifier: r.qualifier[:meth], min_age: r.qualifier[:min_age], max_age: r.qualifier[:max_age])
-end
+RateCategory.where(name: "assigned_0_17").first.update(
+  qualifier: "assigned_0_17",
+  min_age: 0,
+  max_age: 17
+)
+
+RateCategory.where(name: "temporary_permit_0_17").first.update(
+  qualifier: "temporary_permit",
+  min_age: 0,
+  max_age: 17
+)
+
+RateCategory.where(name: "temporary_permit_18_20").first.update(
+  qualifier: "temporary_permit",
+  min_age: 18,
+  max_age: 20
+)
+
+RateCategory.where(name: "residence_permit_0_17").first.update(
+  qualifier: "residence_permit",
+  min_age: 0,
+  max_age: 17
+)
+
+RateCategory.where(name: "residence_permit_18_20").first.update(
+  qualifier: "residence_permit",
+  min_age: 18,
+  max_age: 20
+)
